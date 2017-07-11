7a0a5cb465594a5951fcae9965695011491187b2
sfrieds3 <sfrieds3@gmail.com>
2017-07-10 21:12:55 -0500

    update init.el

--- init.el
+++ init.el
@@ -146,6 +146,8 @@
 (use-package auto-complete
   :ensure t)
 (ac-config-default)
+(setq ac-auto-show-menu 0)   ; show menu immediately
+(setq ac-ignore-case 'smart) ; use smart case
 (global-auto-complete-mode t)
 
 ;; yasnippet
