d5f33ad49e3b3db6431618400bbe7f207c834285
sfrieds3 <sfrieds3@gmail.com>
2017-09-16 16:06:15 -0500

    update setup file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#update git color for untracked files to white
+# update git color for untracked files to white
 git config color.status.untracked white
 git config color.status.changed "white normal dim"
 git config color.status.nobranch "red bold ul"
