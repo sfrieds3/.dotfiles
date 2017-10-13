20c8a156a7976d268ff46bd2bba89ae60faacca6
Scott Friedrich <sfrieds3@gmail.com>
2017-10-13 17:51:05 -0500

    update setup.sh for git diff colors

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@
 git config --global color.status.untracked white
 git config --global color.status.changed "white normal dim"
 git config --global color.status.nobranch "red bold ul"
+
+# update git color for diff
+git config --global color.diff.old "yellow reverse dim"
