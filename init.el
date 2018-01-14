597781098d8051eda309ccd3fabf50a7e486b957
sfrieds3 <sfrieds3@gmail.com>
2018-01-14 10:35:53 -0600

    clean up package archives

--- init.el
+++ init.el
@@ -12,9 +12,7 @@
 (require 'package)
 
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-;;(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
 (package-initialize)
