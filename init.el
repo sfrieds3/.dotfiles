98a3be30d600eac95c61a151368b3695c2c8c899
sfrieds3 <sfrieds3@gmail.com>
2018-04-18 18:56:14 -0500

    update init.el

--- init.el
+++ init.el
@@ -159,13 +159,6 @@
   'interactive)
 (global-set-key "\M-z" 'zap-up-to-char)
 
-;; key-chords
-;; (defun jc/switch-to-previous-buffer ()
-;;   (interactive)
-;;   (switch-to-buffer (other-buffer (current-buffer) 1)))
-
-;; (key-chord-define-global "JJ" 'jc/switch-to-previous-buffer)
-
 ;; ////////////////////////////////////////////////////////////
 
 ;;;; PACKAGES
@@ -237,12 +230,31 @@
          ("C-M-p" . dumb-jump-back)
          ("C-M-q" . dumb-jump-quick-look)))
 
-;; ido
-(use-package ido
-  :ensure t
-  :init
-  (ido-mode t)
-  (ido-everywhere 1))
+;; ivy
+(use-package counsel
+  :ensure t)
+(use-package ivy :demand
+  :config
+  (setq ivy-use-virtual-buffers t
+        ivy-count-format "%d/%d "))
+(counsel-mode 1)
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+(global-set-key (kbd "C-s") 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
 
 ;; smartparens
 (use-package smartparens
