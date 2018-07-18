3d288b0e24df5d23d72741b40a1901fe04aa795e
sfrieds3 <sfrieds3@gmail.com>
2018-07-18 18:16:34 -0500

    update setup scripts

---
+++ apt_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TO_APT_INSTALL='neovim python-neovim python3-neovim pip pip3 exuberant-ctags'
+
+apt update
+#apt install -y $TO_APT_INSTALL
--- setup.py
+++ setup.py
@@ -1,32 +1,26 @@
 #! /usr/bin/python3
 import os
 import subprocess
+from os.path import expanduser
+
+user_home = expanduser("~")
 
 # symlink bash, ctags, gitignore, vimrc
 bash_files = ['bashrc', 'bash_aliases', 'ctags', 'dircolors', 'gitignore', 'inputrc', 'vimrc']
 
 for x in bash_files:
-        if not os.path.islink('/home/scott/.' + x):
-                os.symlink('/home/scott/.dotfiles/' + x, '/home/scott/.' + x)
+        if not os.path.islink(user_home + '/.' + x):
+                os.symlink(user_home + '/.dotfiles/' + x, user_home + '/.' + x)
 
-if not os.path.islink('/home/scott/.config/nvim/init.vim'):
-        os.symlink('/home/scott/.dotfiles', '/home/scott/.congif/nvim/init.vim')
+if not os.path.islink(user_home + '/.config/nvim/init.vim'):
+        os.symlink(user_home + '/.dotfiles', user_home + '/.congif/nvim/init.vim')
 
 # I'm lazy, shell script to set up to set custom colors for git
-subprocess.call('/home/scott/.dotfiles/setup.sh')
+subprocess.call(user_home + '/.dotfiles/setup.sh')
 
-# install and set up tmux
+# I'm still lazy.. shell script to apt install neovim etc etc
+print(user_home + '/.dotfiles/apt_install.sh')
+subprocess.call(user_home + '/.dotfiles/apt_install.sh')
 
-# install neovim
+# install golang
 
-# vim pre-req's
-
-# install spotify
-
-# install dropbox
-
-# install vs code
-
-# install simplenote
-
-# install wireshark
