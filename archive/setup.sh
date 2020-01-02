5b750cf6e48ff043f7c0531a04d34e9b09c47e60
scott <s@thisnullptr.com>
2020-01-02 16:31:03 -0600

    update setup shell script

--- archive/setup.sh
+++ archive/setup.sh
@@ -29,9 +29,6 @@ export LS_COLORS
 # apt install packages for ubuntu
 sudo apt install exuberant-ctags
 sudo apt install leiningen
-sudo apt install neovim
-sudo apt install python-neovim
-sudo apt install python3-neovim
 sudo apt install postgresql
 sudo apt install tmux
 sudo apt install mysql-server
@@ -41,14 +38,23 @@ sudo apt install meld
 sudo apt install gitk
 sudo apt install vim-nox
 sudo apt install libxml2-utils
+sudo apt install dconf-tools
+sudo apt install sbcl
+sudo apt install gnome-tweaks
+sudo snap install emacs
+sudo snap install spotify --classic
 
 # python pip installation
-python3 -m pip install jedi
-python3 -m pip install neovim
-python -m pip install neovim
-python3 -m pip install websocket-client sexpdata
-python3 -m pip install virtualenv
+#python3 -m pip install jedi
+#python3 -m pip install websocket-client sexpdata
+#python3 -m pip install virtualenv
 
 # add vimrc and tmux.conf
+mkdir ~/.emacs.d
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/init.el ~/.emacs.d/init.el
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/gitattributes ~/.gitattributes
+ln -s ~/.dotfiles/inputrc ~/.inputrc
