df2b09ad06e1300117ffc25ddd1a418c1ded52ac
scott <s@thisnullptr.com>
2020-01-09 19:58:04 -0600

    remove some stuff

--- init.el
+++ init.el
@@ -6,12 +6,6 @@
 ;;; Code:
 ;;;; GENERAL PACKAGE SETTINGS
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (eval-when-compile
   (add-to-list 'load-path "~/.emacs.d/elpa")
   (add-to-list 'load-path "~/.emacs.d/elisp"))
