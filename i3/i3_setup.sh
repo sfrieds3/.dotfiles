38b55b21b2483a43501ea758e0c01137b4352cc2
scwfri <scwfri@gmail.com>
2020-08-16 12:05:03 -0500

    update i3 stuff

--- i3/i3_setup.sh
+++ i3/i3_setup.sh
@@ -1,9 +1,13 @@
 #/usr/bin/sh
 
 mkdir -p ~/.i3/scripts
-
 ln -s ~/.dotfiles/lock_and_blur.sh ~/.i3/scripts/lock
+
+# TODO
+# ln -s ~/.dotfiles/i3 ~/.i3/
 
 sudo apt install imagemagick
 sudo apt install scrot
 sudo apt install xautolock
+sudo apt install light
+sudo apt install playerctl
