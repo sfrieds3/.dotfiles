a013187949c902f4d4640e9849a92ea10c4c0590
scwfri <scwfri@gmail.com>
2020-09-01 17:20:00 -0500

    add mutt to setup script

--- setup.sh
+++ setup.sh
@@ -40,7 +40,6 @@ sudo apt install -y kdiff3
 sudo apt install -y gitk
 sudo apt install -y rawtherapee
 sudo apt install -y vim-nox
-sudo apt build-dep vim
 sudo apt install -y libxml2-utils
 sudo apt install -y dconf-tools
 sudo apt install -y sbcl
@@ -50,8 +49,8 @@ sudo apt install -y git
 sudo apt install -y curl
 sudo apt install -y cmake
 sudo apt install -y fonts-font-awesome
-sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
 sudo apt install -y libreoffice
+sudo apt install -y mutt
 sudo snap install spotify --classic
 sudo snap install datagrip --classsic
 
