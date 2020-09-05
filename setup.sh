596fb483dde234f9977a305150345a1477936fd3
scwfri <scwfri@gmail.com>
2020-09-05 15:47:35 -0500

    update setup script

--- setup.sh
+++ setup.sh
@@ -57,6 +57,7 @@ sudo apt install -y net-tools
 sudo apt install -y ufw
 sudo apt install -y openssh-server
 sudo apt install -y flatpak
+sudo apt install -y xdg-desktop-portal
 
 sudo snap install spotify --classic
 sudo snap install datagrip --classsic
@@ -65,18 +66,6 @@ sudo snap install datagrip --classsic
 sudo ufw enable
 sudo sfw logging on
 sudo ufw allow ssh
-
-# add flatpak source
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# install sbt
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
-sudo apt-get update
-sudo apt-get install sbt
-
-# python pip installation
-/usr/bin/python3 -m pip install pip
 
 echo "Checking to see if $codedir is already created"
 if ! [ -d "$codedir" ]; then
@@ -108,5 +97,20 @@ do
     echo "running: ln -s $dotfiles/$file $HOME/.$file..."
     ln -s $dotfiles/$file $HOME/.$file
 done
+
+# python pip installation
+/usr/bin/python3 -m pip install pip
+
+# add flatpak source
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# install sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
+sudo apt-get update
+sudo apt-get install sbt
+
+# install feed reader
+flatpak install flathub org.gnome.FeedReader
 
 source ~/.bashrc
