5b98a35b299079951310b787ab476cd82f70bbf7
scwfri <scwfri@gmail.com>
2020-09-05 15:31:43 -0500

    additional setup

--- setup.sh
+++ setup.sh
@@ -54,9 +54,13 @@ sudo apt install -y mutt
 sudo apt install -y chicken-bin
 sudo apt install -y guile-3.0
 sudo apt install -y net-tools
+sudo apt install -y ufw
 
 sudo snap install spotify --classic
 sudo snap install datagrip --classsic
+
+sudo ufw enable
+sudo sfw logging on
 
 # install sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
