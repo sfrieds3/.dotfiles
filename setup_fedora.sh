04f802a85d78e62ec2eb22f6d912b5e3426c1a9f
scwfri <scwfri@gmail.com>
2020-11-05 17:07:17 -0600

    add node installation

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -156,4 +156,8 @@ sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
 sudo sh -c 'echo -e "[1password]\nname=1Password\nbaseurl=https://downloads.1password.com/linux/rpm\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://downloads.1password.com/linux/keys/1password.asc" > /etc/yum.repos.d/1password.repo'
 sudo dnf install 1password
 
+# install nodejs
+curl -sL https://rpm.nodesource.com/setup_15.x | sudo bash -
+sudo yum install -y nodejs
+
 source ~/.bashrc
