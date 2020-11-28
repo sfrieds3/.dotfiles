6b878f5bab54822add42a213860a77f1c73c65a2
scwfri <scwfri@gmail.com>
2020-11-28 12:18:27 -0600

    add mercurial to setup script

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -37,6 +37,7 @@ sudo dnf install -y postgresql
 sudo dnf install -y postgresql-server
 sudo dnf install -y gparted
 sudo dnf install -y scala
+sudo dnf install -y mercurial
 sudo dnf groupinstall "Development Tools"
 
 # cinnamon installs vim-powerline, which we dont want
