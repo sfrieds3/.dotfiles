12b922b02de519fd422afdafc03e37a14b38bc03
scwfri <scwfri@gmail.com>
2021-05-29 11:33:01 -0500

    Update neovim config, add lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,8 @@
-vim.cmd[[set runtimepath^=~/.vim runtimepath+=~/.vim/after runtimepath+=~/.config/nvim/colors]]
+vim.cmd[[set runtimepath^=~/.vim runtimepath+=~/.vim/after runtimepath+=~/.config/nvim/colors runtimepath+=~/.config/nvim/pack]]
 vim.cmd[[let &packpath = &runtimepath]]
 vim.cmd[[set shell=/usr/bin/zsh]]
 vim.cmd[[source ~/.vim/vimrc]]
 
 require('scwfri.colors')
 require('scwfri.config')
+require('scwfri.plugin')
