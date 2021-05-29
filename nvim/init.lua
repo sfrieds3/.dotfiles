d8029f47e0df7afb6a0b0ab9de0c75120faa0e5b
scwfri <scwfri@gmail.com>
2021-05-29 11:38:24 -0500

    move around some stuff for nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,4 @@
-vim.cmd[[set runtimepath^=~/.vim runtimepath+=~/.vim/after runtimepath+=~/.config/nvim/colors runtimepath+=~/.config/nvim/pack]]
-vim.cmd[[let &packpath = &runtimepath]]
-vim.cmd[[set shell=/usr/bin/zsh]]
-vim.cmd[[source ~/.vim/vimrc]]
-
-require('scwfri.colors')
+-- load configs
 require('scwfri.config')
+require('scwfri.colors')
 require('scwfri.plugin')
--- nvim/lua/scwfri/config.lua
+++ nvim/lua/scwfri/config.lua
@@ -1,3 +1,7 @@
+vim.cmd[[set runtimepath^=~/.vim runtimepath+=~/.vim/after runtimepath+=~/.config/nvim/colors runtimepath+=~/.config/nvim/pack]]
+vim.cmd[[let &packpath = &runtimepath]]
+vim.cmd[[set shell=/usr/bin/zsh]]
+vim.cmd[[source ~/.vim/vimrc]]
 
 vim.cmd[[nnoremap \ev :vsplit $MYVIMRC<cr>]]
 vim.cmd[[nnoremap \es :luafile $MYVIMRC<cr> :echo 'sourced ' . $MYVIMRC<cr>]]
