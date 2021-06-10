363915c3c9d6ce14b8368e36fdb665979cdd38f3
scwfri <1473181+scwfri@users.noreply.github.com>
2021-06-10 14:25:06 -0500

    Update colors.lua

--- nvim/lua/scwfri/colors.lua
+++ nvim/lua/scwfri/colors.lua
@@ -1,2 +1,7 @@
 vim.g.tokyonight_style = "night"
-vim.cmd[[colorscheme monokai]]
+vim.g.material_style = 'oceanic'
+vim.g.material_borders = true
+vim.g.material_contrast = true
+vim.g.sonokai_style = 'espresso'
+
+vim.cmd[[colorscheme sonokai]]
