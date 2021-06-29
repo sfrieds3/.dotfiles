344132d4fb166202f02b8661d57c37f1dad49e1d
scwfri <1473181+scwfri@users.noreply.github.com>
2021-06-29 13:45:17 -0500

    Update utils.lua

--- nvim/lua/scwfri/utils.lua
+++ nvim/lua/scwfri/utils.lua
@@ -3,9 +3,14 @@ local M = {}
 M.functions = {}
 
 function M.execute(id)
-  local fun = M.functions[id]
-  if not functionsc then 
+  local func = M.functions[id]
+  if not func then 
     error("Function does not exist: " .. id)
   end
   return func()
+end
+
+function _G.dump(...)
+  local objects = vim.tbl_map(vim.inspect, {...})
+  print(unpack(objects))
 end
