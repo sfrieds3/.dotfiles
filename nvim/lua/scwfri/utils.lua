c8c1f5f0ee866bcb6541971daff54b939c60ce5c
scwfri <scwfri@gmail.com>
2021-07-05 16:32:04 -0500

    update utils.lua

--- nvim/lua/scwfri/utils.lua
+++ nvim/lua/scwfri/utils.lua
@@ -1,21 +1,13 @@
 local M = {}
 
-M.functions = {}
-
-function M.execute(id)
-  local func = M.functions[id]
-  if not func then 
-    error("Function does not exist: " .. id)
-  end
-  return func()
-end
-
 function _G.dump(...)
   local objects = vim.tbl_map(vim.inspect, {...})
   print(unpack(objects))
 end
 
-local function M.t(str)
+function M.t(str)
     -- Adjust boolean arguments as needed
     return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
+
+return M
