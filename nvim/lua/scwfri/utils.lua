bb71168947aa82c8ceff86117a3e580cd588a69b
scwfri <1473181+scwfri@users.noreply.github.com>
2021-07-05 07:12:17 -0500

    Update utils.lua

--- nvim/lua/scwfri/utils.lua
+++ nvim/lua/scwfri/utils.lua
@@ -14,3 +14,8 @@ function _G.dump(...)
   local objects = vim.tbl_map(vim.inspect, {...})
   print(unpack(objects))
 end
+
+local function M.t(str)
+    -- Adjust boolean arguments as needed
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
