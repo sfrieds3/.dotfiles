1b9dc03d1f7735eaf4c4fdd3b85b132f906995e4
scwfri <1473181+scwfri@users.noreply.github.com>
2021-07-12 15:36:30 -0500

    Update config.lua

--- nvim/lua/scwfri/config.lua
+++ nvim/lua/scwfri/config.lua
@@ -1,6 +1,16 @@
 vim.cmd([[ 
   source ~/.vim/vimrc
-]])
+
+  " neovim specific stuff
+  set termguicolors
+  augroup Neovim
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+    augroup END
+
+  " packadd cfilter
+  packadd cfilter
+  ]])
 
 vim.opt.showmode = false
 vim.o.inccommand = "split"
