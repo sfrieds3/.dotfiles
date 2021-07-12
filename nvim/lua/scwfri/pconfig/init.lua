7af5e6184f4857d284689ef0558f3f428f990624
scwfri <scwfri@gmail.com>
2021-07-11 21:40:10 -0500

    add norg

--- nvim/lua/scwfri/pconfig/init.lua
+++ nvim/lua/scwfri/pconfig/init.lua
@@ -9,6 +9,7 @@ require('scwfri.pconfig.snap_config')
 require('scwfri.pconfig.neogit_config')
 require('scwfri.pconfig.orgmode_config')
 require('scwfri.pconfig.luasnip_config')
+require('scwfri.pconfig.neorg_config')
 
 require('scwfri.pconfig.lsp_config')
 require('scwfri.pconfig.nlua_config')
