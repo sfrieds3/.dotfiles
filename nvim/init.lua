1880692d28bb49e027d983febd5fddd94f05e569
scwfri <1473181+scwfri@users.noreply.github.com>
2021-07-16 10:25:49 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,12 @@
+-- load pathogen
+local config_path = vim.fn.stdpath("config")
+local pathogen_path = string.format("%s/autoload/pathogen.vim", config_path)
+if vim.fn.filereadable(vim.fn.glob(pathogen_path)) then
+    vim.fn['pathogen#infect']('pack/bundle/start/{}')
+    vim.fn['pathogen#infect']('pack/bundle/opt/{}')
+    vim.fn['pathogen#infect']('colors/{}')
+    vim.fn['pathogen#helptags']()
+end
+
 -- load configs
-vim.cmd [[
-if filereadable(glob('$HOME/.vim/autoload/pathogen.vim'))
-    execute pathogen#infect('pack/bundle/start/{}')
-    execute pathogen#infect('pack/bundle/opt/{}')
-    execute pathogen#infect('colors/{}')
-    execute pathogen#helptags()
-endif
-]]
 require('scwfri')
