38f3c88850326888d1f3ec79604937a4005dedca
scwfri <scwfri@gmail.com>
2021-07-15 22:24:23 -0500

    Add pathogen to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,10 @@
 -- load configs
+vim.cmd [[
+if filereadable(glob('$HOME/.vim/autoload/pathogen.vim'))
+    execute pathogen#infect('pack/bundle/start/{}')
+    execute pathogen#infect('pack/bundle/opt/{}')
+    execute pathogen#infect('colors/{}')
+    execute pathogen#helptags()
+endif
+]]
 require('scwfri')
