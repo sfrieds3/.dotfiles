cc92abbc9fbaffcff44da1ba4e3d3ea982d5f09b
scwfri <1473181+scwfri@users.noreply.github.com>
2021-07-26 11:23:52 -0500

    Update telescope_config.lua

--- nvim/lua/scwfri/pconfig/telescope_config.lua
+++ nvim/lua/scwfri/pconfig/telescope_config.lua
@@ -23,7 +23,7 @@ require('telescope').setup {
     file_sorter =  require'telescope.sorters'.get_fuzzy_file,
     file_ignore_patterns = {},
     generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
-    path_display = { "shorten", shorten = 3 },
+    path_display = { "absolute" },
     winblend = 0,
     layout_config = {
       width = 0.75,
