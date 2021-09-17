c4a304058a776b19ce5dfb902cf3c6dd13edad89
scwfri <scwfri@gmail.com>
2021-09-16 22:08:02 -0500

    move from nvim-compe to nvim-cmp, and luasnip to vsnip

--- nvim/lua/pconfig/init.lua
+++ nvim/lua/pconfig/init.lua
@@ -1,9 +1,10 @@
 -- packages requiring configuration
-require('pconfig.compe_config')
+require('pconfig.cmp_config')
 require('pconfig.gitsigns_config')
 require('pconfig.bqf_config')
 require('pconfig.colorizer_config')
-require('pconfig.luasnip_config')
+--require('pconfig.luasnip_config')
+require('pconfig.vsnip_config')
 require('pconfig.markdown_config')
 
 require('pconfig.lsp_config')
