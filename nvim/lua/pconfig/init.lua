422f95cb7d8e80d2bcf33538a62aa79b71cbd274
scwfri <scwfri@gmail.com>
2021-10-28 16:30:07 -0500

    remove nvim-bqf

--- nvim/lua/pconfig/init.lua
+++ nvim/lua/pconfig/init.lua
@@ -1,6 +1,5 @@
 -- packages requiring configuration
 require('pconfig.cmp_config')
 require('pconfig.gitsigns_config')
-require('pconfig.bqf_config')
 require('pconfig.vsnip_config')
 require('pconfig.lsp_config')
