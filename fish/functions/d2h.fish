c0bd44a14d01b7ac93e789bfd889d4a54123f5fe
scwfri <scwfri@gmail.com>
2021-11-20 17:21:43 -0600

    add some fish functions

---
+++ fish/functions/d2h.fish
@@ -0,0 +1,4 @@
+function d2h
+    echo "obase=16; $argv[1]" | bc
+end
+
---
+++ fish/functions/epoch.fish
@@ -0,0 +1,7 @@
+function epoch --description 'Given a number of nanos since epoch, output a UNIX UTC timestamp' --argument epoch_time
+  if echo $argv | grep -v '[:-]' >/dev/null
+        date -u -d @(math "$argv[1] / 1000000000")
+  else
+    date -u -d "$argv" '+%s'000000000
+  end
+end
