10ea3130268bbe7f01035a12c248b4636488d173
scwfri <scwfri@gmail.com>
2022-01-03 16:40:25 -0600

    Fish: Update fish path

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-#fish_add_path ~/local/bin
-contains $HOME/local/bin $fish_user_paths; or set -U fish_user_paths $HOME/local/bin
-#fish_add_path ~/lib
-contains $HOME/lib $fish_user_paths; or set -U fish_user_paths $HOME/lib
-#fish_add_path ~/bin
-contains $HOME/bin $fish_user_paths; or set -U fish_user_paths $HOME/bin
-
 # aliases
 alias fd="fdfind"
 alias gap="git add --patch"
