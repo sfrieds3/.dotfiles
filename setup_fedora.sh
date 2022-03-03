5e07e2626f45dd91a54cd8e778e4d0bfe174b7b4
scwfri <scwfri@gmail.com>
2022-03-03 10:07:32 -0600

    All: Update setup_fedora, zshenv
    Remove unnecessary commands from setup_fedora
    Add rbenv path to zshenv, put cargo load in if block
    Add go to path

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -16,20 +16,15 @@ dotfiles=$HOME/.dotfiles
 #  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 dnf_install=(
-    @cinnamon-desktop neovim git ripgrep fd-find bat
-    fzf gitk clang clang-tools clang-tools-extra htop
-    ShellCheck llvm libgcrypt-devel git-core automake
-    binutils-devel curl cmake mpfr-devel libmpc-devel
-    gmp-devel e2f sprogs ninja-build patch ccache rsync
-    @"C Development Tools and Libraries" @Virtualization
-    git-core zlib zlib-devel gcc-c++ patch readline
-    readline-devel libyaml-devel libffi-devel openssl-devel
-    make bzip2 autoconf automake libtool bison curl 
-    sqlite-devel perl-core guile chicken gcc lua
-    java-11-openjdk-devel
+    neovim git ripgrep fd-find bat fzf gitk clang clang-tools clang-tools-extra
+    htop ShellCheck llvm libgcrypt-devel git-core automake binutils-devel curl
+    cmake mpfr-devel libmpc-devel gmp-devel e2f sprogs ninja-build patch ccache
+    rsync @"C Development Tools and Libraries" @Virtualization git-core zlib
+    zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel
+    openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel
+    perl-core guile chicken gcc lua java-11-openjdk-devel texinfo ruby-devel
+    gimp
 )
-
-group_install=( "Development Tools" "Development Libraries" )
 
 # python build deps
 sudo dnf builddep python3
@@ -39,60 +34,48 @@ do
     sudo dnf install -y $i
 done
 
-for i in "${group_install[@]}"
-do
-    sudo dnf groupinstall $i
-done
-
-# cinnamon installs vim-powerline, which we dont want
-# which then removes vim-enhanced... ugh
-sudo dnf remove -y vim-powerline
-sudo dnf install -y vim-enhanced
-
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y --noninteractive flathub com.spotify.Client
 flatpak install -y --noninteractive flathub org.videolan.VLC
 flatpak install -y --noninteractive flathub com.obsproject.Studio
 flatpak install -y --noninteractive flathub us.zoom.Zoom
-flatpak install -y --noninteractive flathub com.jetbrains.IntelliJ-IDEA-Ultimate
-flatpak install -y --noninteractive flathub com.jetbrains.DataGrip
 
 # enable ssh
 # https://docs.fedoraproject.org/en-US/fedora/rawhide/system-administrators-guide/infrastructure-services/OpenSSH/#s2-ssh-configuration-sshd
-sudo systemctl enable sshd.service
+#sudo systemctl enable sshd.service
+#
+#sudo systemctl disable telnet.service
+#sudo systemctl disable rsh.service
+#sudo systemctl disable rlogin.service
+#sudo systemctl disable vsftpd.service
 
-sudo systemctl disable telnet.service
-sudo systemctl disable rsh.service
-sudo systemctl disable rlogin.service
-sudo systemctl disable vsftpd.service
+#echo "Checking to see if $codedir is already created"
+#if ! [ -d "$codedir" ]; then
+#    echo "it is not... calling mkdir $codedir"
+#    mkdir $codedir
+#fi
+#
+#if [ -d "$vimdir" ]; then
+#    echo "$vimdir already exists.. moving to $vimdir.$datetime"
+#    mv $vimdir $vimdir.$datetime
+#    echo "cloning vimconfig: git clone git@github.com:scwfri/vimconfig $vimdir"
+#    git clone git@github.com:scwfri/vimconfig $vimdir
+#fi
+#
+#for file in tmux.conf bashrc csirc gitignore tmux.statusline gitconfig zshenv ctags sbclrc inputrc ripgreprc
+#do
+#    if [ -f "$HOME/.$file" ]; then
+#        echo "$HOME/$file already exists.. moving to $HOME/.$file.$datetime"
+#    fi
+#
+#    echo "running: ln -s $dotfiles/$file $HOME/.$file..."
+#    ln -s $dotfiles/$file $HOME/.$file
+#done
 
-echo "Checking to see if $codedir is already created"
-if ! [ -d "$codedir" ]; then
-    echo "it is not... calling mkdir $codedir"
-    mkdir $codedir
-fi
-
-if [ -d "$vimdir" ]; then
-    echo "$vimdir already exists.. moving to $vimdir.$datetime"
-    mv $vimdir $vimdir.$datetime
-    echo "cloning vimconfig: git clone git@github.com:scwfri/vimconfig $vimdir"
-    git clone git@github.com:scwfri/vimconfig $vimdir
-fi
-
-for file in tmux.conf bashrc csirc gitignore tmux.statusline gitconfig zshenv ctags sbclrc inputrc ripgreprc
-do
-    if [ -f "$HOME/.$file" ]; then
-        echo "$HOME/$file already exists.. moving to $HOME/.$file.$datetime"
-    fi
-
-    echo "running: ln -s $dotfiles/$file $HOME/.$file..."
-    ln -s $dotfiles/$file $HOME/.$file
-done
-
-if ! [ -d "$XDG_CONFIG_HOME/lib-scwfri" ]; then
-    echo "ln -s $HOME/.dotfiles/lib-scwfri $XDG_CONFIG_HOME/lib-scwfri"
-    ln -s $HOME/.dotfiles/lib-scwfri $XDG_CONFIG_HOME
-fi
+#if ! [ -d "$XDG_CONFIG_HOME/lib-scwfri" ]; then
+#    echo "ln -s $HOME/.dotfiles/lib-scwfri $XDG_CONFIG_HOME/lib-scwfri"
+#    ln -s $HOME/.dotfiles/lib-scwfri $XDG_CONFIG_HOME
+#fi
 
 #if ! [ -d "$XDG_CONFIG_HOME/zsh" ]; then
 #    echo "ln -s $HOME/.dotfiles/zsh $XDG_CONFIG_HOME/zsh"
@@ -105,6 +88,8 @@ fi
 #fi
 
 # python pip installation
+/usr/bin/python3 -m ensurepip
+/usr/bin/python3 -m pip install --upgrade pip
 /usr/bin/python3 -m pip install --user 'python-lsp-server[all]'
 /usr/bin/python3 -m pip install --user autopep8
 /usr/bin/python3 -m pip install --user pylint
@@ -115,24 +100,24 @@ fi
 /usr/bin/python3 -m pip install --user pre-commit
 
 # install rbenv and ruby-build
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-mkdir -p "$(rbenv root)"/plugins
-git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+#git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+#mkdir -p "$(rbenv root)"/plugins
+#git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
 # install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # install sbt
-curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
-sudo dnf install sbt
+#curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo
+#sudo dnf install sbt
 
 # install linenoise for chicken
-CSC_OPTIONS='-I/usr/include/chicken' chicken-install linenoise -s
+#CSC_OPTIONS='-I/usr/include/chicken' chicken-install linenoise -s
 
 # install quicklisp
-curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
-sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp \
-       --eval '(quicklisp-quickstart:install :path "~/.quicklisp")' \
-       --eval '(ql:add-to-init-file)' \
-       --quit
-
+#curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
+#sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp \
+#       --eval '(quicklisp-quickstart:install :path "~/.quicklisp")' \
+#       --eval '(ql:add-to-init-file)' \
+#       --quit
+#
--- zshenv
+++ zshenv
@@ -5,7 +5,8 @@ export ZDOTDIR=$XDG_CONFIG_HOME/zsh
 export HISTFILE=$ZDOTDIR/zsh_history
 
 # set path and stuff
-export PATH=$HOME/lib:$HOME/bin:$HOME/.local/bin:$XDG_CONFIG_HOME/lib-scwfri:$HOME/.luarocks/bin:$PATH:/$HOME/perl5/bin
+export PATH=$HOME/lib:$HOME/bin:$HOME/.local/bin:$XDG_CONFIG_HOME/lib-scwfri:$HOME/.luarocks/bin:$PATH:/$HOME/perl5/bin:/$HOME/.rbenv/bin:/usr/local/go/bin
+fpath=($XDG_CONFIG_HOME/zsh $fpath)
 export PERL5LIB="/$HOME/perl5/lib/perl5"
 export PERL_LOCAL_LIB_ROOT="/$HOME/perl5"
 export PERL_MB_OPT="--install_base \"/$HOME/perl5\""
@@ -18,8 +19,10 @@ export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc
 export JDTLS_HOME=$HOME/bin/jdtls/
 export FZF_DEFAULT_COMMAND="rg --files"
 
-fpath=($XDG_CONFIG_HOME/zsh $fpath)
-. "$HOME/.cargo/env"
+
+if [ -f "#HOME/.cargo/env" ]; then
+    . "$HOME/.cargo/env"
+fi
 
 if [[ $TERM =~ konsole.* ]]; then
     export FZF_DEFAULT_OPTS='--color fg+:5,hl+:6'
