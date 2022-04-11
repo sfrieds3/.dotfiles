b21e5c5db2fa46823ddd2af7971954e7a3464e6a
scwfri <scwfri@gmail.com>
2022-04-11 16:56:42 -0500

    Fedora: Add vim-language-server to fedora setup

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -105,6 +105,7 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 # install python pyright language server
 npm install -g pyright
+npm install -g vim-language-server
 
 # install rbenv and ruby-build
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
