cbb1d6539df983807cc931a42ce0d269eaafb6c3
Scott F <1473181+scwfri@users.noreply.github.com>
2022-05-03 09:15:58 -0500

    Update init.lua

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,23 +3,23 @@ vim.g.do_filetype_lua = 1
 local g = vim.g
 local cmd = vim.cmd
 local o, wo, bo = vim.o, vim.wo, vim.bo
-local vim_config_path = string.format('%s/.vim', os.getenv('HOME'))
-local data_dir = vim.fn.stdpath('data')
-local config_path = vim.fn.stdpath('config')
+local vim_config_path = string.format("%s/.vim", os.getenv("HOME"))
+local data_dir = vim.fn.stdpath("data")
+local config_path = vim.fn.stdpath("config")
 
-vim.g.python3_host_prog = '$PYTHON3_VENV'
+vim.g.python3_host_prog = "$PYTHON3_VENV"
 
-require('plugins')
-local utils = require('utils')
+require("plugins")
+local utils = require("utils")
 local set = vim.opt
 
-if vim.fn.executable('rg') then
-  set.grepprg = 'rg -HS --no-heading --hidden --vimgrep'
+if vim.fn.executable("rg") then
+  set.grepprg = "rg -HS --no-heading --hidden --vimgrep"
 else
-  set.grepprg='git grep -in $*'
+  set.grepprg="git grep -in $*"
 end
 
-set.shell = '/usr/bin/zsh'
+set.shell = "/usr/bin/zsh"
 set.termguicolors = true
 set.hidden = true
 set.autoread = true
@@ -46,18 +46,19 @@ set.softtabstop = 4
 set.scrolloff = 3
 
 set.laststatus = 3
-set.backspace = 'indent,eol,start'
-set.encoding = 'utf8'
-set.fileencoding = 'utf8'
+set.backspace = "indent,eol,start"
+set.encoding = "utf8"
+set.fileencoding = "utf8"
 set.showtabline = 3
-set.formatoptions = 'qrn1j'
-set.mouse = 'a'
+set.formatoptions = "qrn1j"
+set.mouse = "a"
 set.redrawtime = 50000
-set.showbreak = '...'
+set.showbreak = "..."
 set.foldenable = false
-set.foldmethod = 'expr'
-set.foldexpr = 'nvim_treesitter#foldexpr()'
-set.foldcolumn = '0'
+set.foldmethod = "expr"
+set.foldexpr = "nvim_treesitter#foldexpr()"
+set.foldcolumn = "0"
+set.signcolumn = "yes"
 
 set.listchars = { tab = '» ', extends = '›', precedes = '‹', nbsp = '␣', trail = '·' }
 set.list = true
@@ -69,16 +70,16 @@ set.ttimeoutlen = 10
 
 set.wildmenu = true
 set.wildignorecase = true
-set.wildignore = '*.o,*.pyc'
+set.wildignore = "*.o,*.pyc"
 set.wildcharm = 26 -- <C-z>
-set.tags = './tags;,tags;'
-set.completeopt = 'menu,menuone,noselect'
-set.path = '.,,'
+set.tags = "./tags;,tags;"
+set.completeopt = "menu,menuone,noselect"
+set.path = ".,,"
 
 set.undofile = true
 set.backup = true
-set.backupdir = vim.opt.backupdir - '.'
-set.backupext = '.bak'
+set.backupdir = vim.opt.backupdir - "."
+set.backupext = ".bak"
 set.swapfile = false
 set.shada = [['1000,f1,<1000,/10000,:10000]]
 
