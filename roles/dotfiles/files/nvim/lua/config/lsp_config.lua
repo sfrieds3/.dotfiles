8c84995fa91866ac53133f197e62bfe66ae3260c
Scott F <scwfri@gmail.com>
2022-08-20 13:00:08 -0500

    Update lsp_config.lua

--- roles/dotfiles/files/nvim/lua/config/lsp_config.lua
+++ roles/dotfiles/files/nvim/lua/config/lsp_config.lua
@@ -41,6 +41,22 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = {
+		'documentation',
+		'detail',
+		'additionalTextEdits',
+	}
+}
+
 local lsp_flags = {
   debounce_text_changes = 150,
 }
