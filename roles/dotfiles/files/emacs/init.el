1ca10f4d05f4ce3cfdaa7171b1c3442eb1540073
scwfri <scwfri@gmail.com>
2022-10-03 10:23:17 -0500

    emacs: config updates

--- roles/dotfiles/files/emacs/init.el
+++ roles/dotfiles/files/emacs/init.el
@@ -700,11 +700,12 @@
   :config
   (consult-customize
    consult-theme
-   :preview-key '(:debounce 0.2 any)
+   :preview-key '(:debounce 0.2 any))
+  (consult-customize
    consult-ripgrep consult-git-grep consult-grep
    consult-bookmark consult-recent-file consult-xref
    consult--source-bookmark consult--source-recent-file
-   consult--source-project-recent-file
+   consult--source-project-recent-file consult-line
    :preview-key (kbd "M-."))
 
   (setf consult-narrow-key (kbd "C-+"))
