4a17406e1266e0ebb4dd3c4ff4cc4805ffeb8656
scwfri <scwfri@gmail.com>
2022-10-23 12:15:02 -0500

    more config updates

--- roles/dotfiles/files/emacs/lisp/scwfri/toggle-commands.el
+++ roles/dotfiles/files/emacs/lisp/scwfri/toggle-commands.el
@@ -32,7 +32,7 @@
 (defun $cycle-font (arg)
   "Cycle through fonts.  ARG is prefix arg, default 1."
   (interactive "p")
-  (let* ((fonts ["VictorMono Nerd Font Mono" "SauceCodePro Nerd Font Mono" "Hack Nerd Font Mono" "FiraMono Nerd Font" "JetBrainsMono Nerd Font"])
+  (let* ((fonts ["VictorMono Nerd Font Mono" "SauceCodePro Nerd Font Mono" "Hack Nerd Font Mono" "JetBrainsMono Nerd Font Mono"])
          (idx (if (get '$cycle-font 'state)
                   (get '$cycle-font 'state)
                 0))
