a1aec7cda86b47758517bb26d1323d6bab8850b8
Scott F <sfrieds3@gmail.com>
2022-11-30 16:42:37 -0600

    update rg command

--- roles/dotfiles/files/nvim/after/plugin/grep.lua
+++ roles/dotfiles/files/nvim/after/plugin/grep.lua
@@ -1,5 +1,5 @@
 if vim.fn.executable("rg") then
-  vim.opt.grepprg = "rg --no-heading --hidden --vimgrep"
+  vim.opt.grepprg = "rg --no-heading --hidden --vimgrep -g '!.git/'"
 else
   vim.opt.grepprg = "git grep -in $*"
 end
