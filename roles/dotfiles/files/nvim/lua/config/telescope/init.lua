8d0f0b4a29ff5dda68408e4ef2eea45a558a0946
Scott F <sfrieds3@gmail.com>
2022-12-01 19:35:16 -0600

    use fd for telescope

--- roles/dotfiles/files/nvim/lua/config/telescope/init.lua
+++ roles/dotfiles/files/nvim/lua/config/telescope/init.lua
@@ -14,7 +14,8 @@ require("telescope").setup({
   },
   pickers = {
     find_files = {
-      find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+      -- find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+      find_command = { "fd", "--hidden", "--absolute-path" },
       layout_config = {
         height = 0.70,
       },
