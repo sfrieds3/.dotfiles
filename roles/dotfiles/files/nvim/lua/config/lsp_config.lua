658a148ea12fffd655e35983bae90b0d4c70599a
Scott F <sfrieds3@gmail.com>
2022-12-12 17:56:56 -0600

    nvim: fix for lua-language-client warning

--- roles/dotfiles/files/nvim/lua/config/lsp_config.lua
+++ roles/dotfiles/files/nvim/lua/config/lsp_config.lua
@@ -216,6 +216,7 @@ if vim.fn.executable("lua-language-server") == 1 then
         workspace = {
           -- Make the server aware of Neovim runtime files
           library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
         },
         -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = {
