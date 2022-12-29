738ff8cb52bc7cc5515d69c7b7c3fe1a904aaeb3
Scott F <sfrieds3@gmail.com>
2022-12-29 16:54:36 -0600

    nvim: update symbols-outline config

--- roles/dotfiles/files/nvim/lua/plugins/symbols_outline.lua
+++ roles/dotfiles/files/nvim/lua/plugins/symbols_outline.lua
@@ -1,7 +1,12 @@
 local M = { "simrat39/symbols-outline.nvim" }
 
 function M.config()
-  require("symbols-outline").setup({})
+  require("symbols-outline").setup({
+    winblend = 10,
+    keymaps = {
+      hover_symbol = "<Leader>e",
+    },
+  })
 
   vim.keymap.set("n", "<Leader><CR>", "<Cmd>SymbolsOutline<CR>")
 end
