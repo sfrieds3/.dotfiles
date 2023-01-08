784a9bdfc7d66c6306f841dfd9cb29a394b66f79
Scott F <sfrieds3@gmail.com>
2023-01-08 08:34:06 -0600

    Update treesitter.lua

--- roles/dotfiles/files/nvim/lua/plugins/treesitter.lua
+++ roles/dotfiles/files/nvim/lua/plugins/treesitter.lua
@@ -95,6 +95,9 @@ return {
               ["if"] = "@function.inner",
               ["ac"] = "@class.outer",
               ["ic"] = "@class.inner",
+              ["al"] = "@loop.outer",
+              ["il"] = "@loop.inner",
+              ["uc"] = "@comment.outer",
             },
           },
           move = {
