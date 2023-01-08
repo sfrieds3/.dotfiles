55c3825e35ccdfe82712e246c300eb2f4ff66b4c
Scott F <sfrieds3@gmail.com>
2023-01-08 08:48:26 -0600

    Update null_ls.lua

--- roles/dotfiles/files/nvim/lua/plugins/lsp/null_ls.lua
+++ roles/dotfiles/files/nvim/lua/plugins/lsp/null_ls.lua
@@ -4,23 +4,25 @@ function M.setup()
   require("null-ls").setup({
     sources = {
       -- code actions
-      require("null-ls").builtins.code_actions.shellcheck,
       require("null-ls").builtins.code_actions.gitsigns,
+      require("null-ls").builtins.code_actions.shellcheck,
 
       -- diagnostics
       require("null-ls").builtins.diagnostics.flake8,
+      require("null-ls").builtins.diagnostics.shellcheck,
       require("null-ls").builtins.diagnostics.trail_space.with({
         filetypes = { git = false },
       }),
-      require("null-ls").builtins.diagnostics.shellcheck,
 
       -- formatting
-      require("null-ls").builtins.formatting.stylua,
       require("null-ls").builtins.formatting.eslint,
-      require("null-ls").builtins.formatting.yapf,
       require("null-ls").builtins.formatting.isort,
       require("null-ls").builtins.formatting.json_tool,
+      require("null-ls").builtins.formatting.stylua,
+      require("null-ls").builtins.formatting.trim_whitespace,
+      require("null-ls").builtins.formatting.trim_newlines,
       require("null-ls").builtins.formatting.xmllint,
+      require("null-ls").builtins.formatting.yapf,
     },
   })
 end
