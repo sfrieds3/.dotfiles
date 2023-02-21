f9c6395eca0fbb85a2d5ef09926171f63bcd6a5d
Scott F <sfrieds3@gmail.com>
2023-02-21 13:26:27 -0600

    chore: update lsp/mason config

--- roles/dotfiles/files/nvim/lua/plugins/lsp/init.lua
+++ roles/dotfiles/files/nvim/lua/plugins/lsp/init.lua
@@ -1,17 +1,21 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    config = function()
-      require("plugins.lsp.lsp").setup()
-    end,
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-      { "nvim-lua/lsp-status.nvim" },
+      "nvim-lua/lsp-status.nvim",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
   },
   {
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
+      local ensure_installed = { "jsonls", "lua-ls", "pyright", "tsserver" }
+      require("mason").setup({ providers = { "mason.providers.client", "mason.providers.registry-api" } })
+      require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
       require("plugins.lsp.null_ls").setup()
+      require("plugins.lsp.lsp").setup()
     end,
   },
 }
