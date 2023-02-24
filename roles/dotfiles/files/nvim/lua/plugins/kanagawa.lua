2d0a534043865f2c9b34b53ba1360b8d776c8fbf
Scott F <sfrieds3@gmail.com>
2023-02-24 17:38:59 -0600

    feat: update config for kanagawa

--- roles/dotfiles/files/nvim/lua/plugins/kanagawa.lua
+++ roles/dotfiles/files/nvim/lua/plugins/kanagawa.lua
@@ -1,8 +1,19 @@
 local M = { "rebelot/kanagawa.nvim", lazy = true }
 
 function M.config()
+  vim.opt.fillchars:append({
+    horiz = "━",
+    horizup = "┻",
+    horizdown = "┳",
+    vert = "┃",
+    vertleft = "┨",
+    vertright = "┣",
+    verthoriz = "╋",
+  })
   require("kanagawa").setup({
     dimInactive = true,
+    globalStatus = true,
+    terminalColors = true,
   })
 end
 
