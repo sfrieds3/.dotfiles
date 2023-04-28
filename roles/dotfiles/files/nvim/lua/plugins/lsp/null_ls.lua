6d22b4713b79a4089e5ae187e7f91f14d662c228
Scott F <sfrieds3@gmail.com>
2023-04-28 11:59:03 -0500

    feat: add djlint to null_ls

--- roles/dotfiles/files/nvim/lua/plugins/lsp/null_ls.lua
+++ roles/dotfiles/files/nvim/lua/plugins/lsp/null_ls.lua
@@ -9,6 +9,7 @@ function M.setup()
 
       --} d}iagnostics
       -- require("null-ls").builtins.diagnostics.flake8,
+      require("null-ls").builtins.diagnostics.djlint,
       require("null-ls").builtins.diagnostics.pylint,
       require("null-ls").builtins.diagnostics.ruff,
       require("null-ls").builtins.diagnostics.shellcheck,
