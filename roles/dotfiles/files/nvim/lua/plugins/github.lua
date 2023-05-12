fd2947bbc149280f36d3d288a3c24daddd2f5856
Scott F <sfrieds3@gmail.com>
2023-05-12 09:57:58 -0500

    feat: updates for alacritty

--- roles/dotfiles/files/nvim/lua/plugins/github.lua
+++ roles/dotfiles/files/nvim/lua/plugins/github.lua
@@ -8,9 +8,10 @@ function M.config()
   require("github-theme").setup({
     theme_style = "dimmed",
     function_style = "italic",
-    sidebars = { "qf", "terminal", "packer" },
+    sidebars = { "qf", "terminal" },
     dark_sidebar = true,
     dark_float = true,
+    dim_inactive = true,
   })
 end
 
