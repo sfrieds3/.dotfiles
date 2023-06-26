4a8b621384c06ef4071b82c61c737baafb5d4b39
Scott F <sfrieds3@gmail.com>
2023-06-26 18:12:58 -0500

    update window picker config, add zsh-completions

--- roles/dotfiles/files/nvim/lua/plugins/window_picker.lua
+++ roles/dotfiles/files/nvim/lua/plugins/window_picker.lua
@@ -1,10 +1,13 @@
 local M = {
   "s1n7ax/nvim-window-picker",
-  version = "v1.*",
+  version = "v2.*",
 }
 
 function M.config()
-  require("window-picker").setup({})
+  require("window-picker").setup({
+    hint = "floating-big-letter",
+  })
+
   vim.keymap.set("n", "<Leader>w", function()
     local winid = require("window-picker").pick_window() or vim.api.nvim_get_current_win()
     vim.api.nvim_set_current_win(winid)
