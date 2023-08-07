95f3a671528a81e6efc1287dea5e4bab771b0e38
Scott F <sfrieds3@gmail.com>
2023-08-06 20:43:57 -0500

    Update path.fish

--- roles/dotfiles/files/fish/conf.d/path.fish
+++ roles/dotfiles/files/fish/conf.d/path.fish
@@ -1,14 +1,14 @@
-# https://github.com/adrg/xdg/blob/master/README.md
-# switch (uname)
-# case "Darwin"
-#   set --global --export XDG_CONFIG_HOME $HOME/Library/Application Support
-#   set --global --export --path XDG_CONFIG_DIRS $HOME/Library/Preferences:/Library/Application Support:/Library/Preferences
-#   set --global --export XDG_DATA_HOME $HOME/Library/Application Support
-#   set --global --export XDG_DATA_DIRS /Library/Application Support
-#   set --global --export XDG_STATE_HOME $HOME/Library/Application Support
-#   set --global --export XDG_CACHE_HOME $HOME/Library/Caches
-#   set --global --export XDG_RUNTIME_DIR $TMPDIR
-# case "*"
+switch (uname)
+case "Darwin"
+  set --global --export XDG_CONFIG_HOME $HOME/.config
+  set --global --export XDG_CONFIG_DIRS /etc/xdg
+  set --global --export XDG_DATA_HOME $HOME/.local/share
+  set --global --export --path XDG_DATA_DIRS /usr/local/share/:/usr/share/
+  set --global --export XDG_STATE_HOME $HOME/.local/state
+  set --global --export XDG_CACHE_HOME $HOME/.cache
+  set --global --export XDG_RUNTIME_DIR $TMPDIR
+  set --global --export HOMEBREW_PREFIX (brew --prefix)
+case "*"
   set --global --export XDG_CONFIG_HOME $HOME/.config
   set --global --export XDG_CONFIG_DIRS /etc/xdg
   set --global --export XDG_DATA_HOME $HOME/.local/share
@@ -17,7 +17,7 @@
   set --global --export XDG_CACHE_HOME $HOME/.cache
   set --global --export XDG_RUNTIME_DIR /run/user/$UID
   set --global --export HOMEBREW_PREFIX (brew --prefix)
-# end
+end
 
 # SET PATH
 set --global --export GOPATH $HOME/go
