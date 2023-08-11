d848aa1faa04fa9bafed2fc7e5cfaf45bba1e705
Scott F <sfrieds3@gmail.com>
2023-08-11 14:41:04 -0500

    Add rbenv to config.fish

--- roles/dotfiles/files/fish/config.fish
+++ roles/dotfiles/files/fish/config.fish
@@ -8,6 +8,9 @@ if status is-interactive
   # pyenv init
   pyenv init - | source
 
+  # rbenv init
+  rbenv init - | source
+
   if test -f $HOMEBREW_PREFIX/Caskroom/miniconda/base/bin/conda
     eval $HOMEBREW_PREFIX/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
   end
