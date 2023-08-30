6ad5b72f84e709f10c90f3311acf7b5c40b81e01
Scott F <sfrieds3@gmail.com>
2023-08-30 07:44:56 -0500

    Update path.fish
    Only set PATH if interactive

--- roles/dotfiles/files/fish/conf.d/path.fish
+++ roles/dotfiles/files/fish/conf.d/path.fish
@@ -19,28 +19,30 @@ case "*"
   set --global --export HOMEBREW_PREFIX (brew --prefix)
 end
 
-# SET PATH
-set --global --export GOPATH $HOME/go
-set --global --export GOBIN $GOPATH/bin
-set LUAROCKSBIN $HOME/.luarocks/bin
-set PERL5BIN $HOME/perl5/bin
-set SYSGOBIN /usr/local/go/bin
-set RBENVBIN $HOME/.rbenv/bin
-set USRLOCALBIN /usr/local/bin
-set LOCALBIN $HOME/.local/bin
-set PYTHON3_VENV_BIN $HOME/.venv/venv/bin
-set OPENJDKBIIN /opt/homebrew/opt/openjdk/bin
-
-set -l pathdirs $LOCALBIN $USRLOCALBIN $GOBIN $SYSGOBIN $PERL5BIN $RBENVBIN $LUAROCKSBIN
-for dir in $pathdirs
-  if not contains -- $dir $PATH
-    fish_add_path $dir
-  end
-end
-
-set -l appendpathdirs $PYTHON3_VENV_BIN $OPENJDKBIN
-for difr in $appendpathdirs
-  if not contains -- $dir $PATH
-    fish_add_path --append $dir
-  end
+# set path if interactive
+if status is-interactive
+	set --global --export GOPATH $HOME/go
+	set --global --export GOBIN $GOPATH/bin
+	set LUAROCKSBIN $HOME/.luarocks/bin
+	set PERL5BIN $HOME/perl5/bin
+	set SYSGOBIN /usr/local/go/bin
+	set RBENVBIN $HOME/.rbenv/bin
+	set USRLOCALBIN /usr/local/bin
+	set LOCALBIN $HOME/.local/bin
+	set PYTHON3_VENV_BIN $HOME/.venv/venv/bin
+	set OPENJDKBIIN /opt/homebrew/opt/openjdk/bin
+	
+	set -l pathdirs $LOCALBIN $USRLOCALBIN $GOBIN $SYSGOBIN $PERL5BIN $RBENVBIN $LUAROCKSBIN
+	for dir in $pathdirs
+	  if not contains -- $dir $PATH
+	    fish_add_path $dir
+	  end
+	end
+	
+	set -l appendpathdirs $PYTHON3_VENV_BIN $OPENJDKBIN
+	for difr in $appendpathdirs
+	  if not contains -- $dir $PATH
+	    fish_add_path --append $dir
+	  end
+	end
 end
