61c445db5133b49be3965635cc4c07ffacac1626
Scott F <sfrieds3@gmail.com>
2023-09-14 18:25:26 -0500

    Close :Git window when closing git commit msg

---
+++ roles/dotfiles/files/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "git" then
+      vim.cmd("quit")
+    end
+  end,
+  group = vim.api.nvim_create_augroup("gitcommit:closenvim", {}),
+})
