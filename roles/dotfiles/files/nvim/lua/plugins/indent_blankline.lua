c31271bea16b5cf170358dad9d0afabce9912ae4
Scott F <sfrieds3@gmail.com>
2023-09-28 09:21:52 -0500

    Update indent_blankline config for v3

--- roles/dotfiles/files/nvim/lua/plugins/indent_blankline.lua
+++ roles/dotfiles/files/nvim/lua/plugins/indent_blankline.lua
@@ -1,9 +1,10 @@
 local M = {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
 }
 
 function M.config()
-  require("indent_blankline").setup({
+  require("ibl").setup({
     space_char_blankline = " ",
     show_current_context = true,
     show_current_context_start = false,
