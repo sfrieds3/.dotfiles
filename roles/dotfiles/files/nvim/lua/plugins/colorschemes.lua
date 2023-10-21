6c58632a984e20c552ebc5b3c72dad045d2280a3
Scott F <sfrieds3@gmail.com>
2023-10-21 18:23:24 -0500

    Move GitHub theme to colorschemes.lua

--- roles/dotfiles/files/nvim/lua/plugins/colorschemes.lua
+++ roles/dotfiles/files/nvim/lua/plugins/colorschemes.lua
@@ -7,6 +7,32 @@ return {
   { "Domeee/mosel.nvim" },
   { "polirritmico/monokai-nightasty.nvim" },
   {
+    "projekt0n/github-nvim-theme",
+    version = "*",
+    config = function()
+      require("github-theme").setup({
+        options = {
+          dim_inactive = true,
+          styles = {
+            functions = "italic",
+          },
+          inverse = { -- Inverse highlight for different types
+            match_paren = true,
+            visual = true,
+            search = true,
+          },
+          darken = {
+            floats = true,
+            sidebars = {
+              enable = true,
+              list = { "qf", "terminal" },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
 
