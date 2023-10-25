8345b310bca33c9bf3ddc43c063c0edc99150e30
Scott F <sfrieds3@gmail.com>
2023-10-25 18:09:11 -0500

    emacs: treemacs -> neotree

--- roles/dotfiles/files/emacs/init.el
+++ roles/dotfiles/files/emacs/init.el
@@ -168,7 +168,6 @@
   :config
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
-  (doom-themes-treemacs-config)
   (doom-themes-org-config))
 
 (use-package blackout
@@ -264,7 +263,7 @@
     "rg" #'deadgrep
     "vd" #'consult-lsp-diagnostics
     "/"  #'consult-line
-    "\\" #'treemacs
+    "\\" #'neotree
     ":" #'consult-lsp-symbols
     ";" #'consult-lsp-file-symbols)
   ($leader
@@ -448,8 +447,6 @@
 
 (use-package lsp-ui
   :commands lsp-ui-mode)
-(use-package lsp-treemacs
-  :commands lsp-treemacs-errors-list)
 
 (use-package lsp-pyright
   :config
@@ -592,112 +589,26 @@
   :after org
   :straight nil)
 
-(use-package treemacs
-  :defer t
+
+(use-package neotree
+  :custom
+  (neo-smart-open t)
+  (projectile-switch-project-action 'neotree-projectile-action)
   :config
-  (treemacs-resize-icons 11)
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-  :config
-  (progn
-    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay        0.5
-          treemacs-directory-name-transformer      #'identity
-          treemacs-display-in-side-window          t
-          treemacs-eldoc-display                   'simple
-          treemacs-file-event-delay                2000
-          treemacs-file-extension-regex            treemacs-last-period-regex-value
-          treemacs-file-follow-delay               0.2
-          treemacs-file-name-transformer           #'identity
-          treemacs-follow-after-init               t
-          treemacs-expand-after-init               t
-          treemacs-find-workspace-method           'find-for-file-or-pick-first
-          treemacs-git-command-pipe                ""
-          treemacs-goto-tag-strategy               'refetch-index
-          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
-          treemacs-hide-dot-git-directory          t
-          treemacs-indentation                     2
-          treemacs-indentation-string              " "
-          treemacs-is-never-other-window           nil
-          treemacs-max-git-entries                 5000
-          treemacs-missing-project-action          'ask
-          treemacs-move-forward-on-expand          nil
-          treemacs-no-png-images                   nil
-          treemacs-no-delete-other-windows         t
-          treemacs-project-follow-cleanup          nil
-          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                        'left
-          treemacs-read-string-input               'from-child-frame
-          treemacs-recenter-distance               0.1
-          treemacs-recenter-after-file-follow      nil
-          treemacs-recenter-after-tag-follow       nil
-          treemacs-recenter-after-project-jump     'always
-          treemacs-recenter-after-project-expand   'on-distance
-          treemacs-litter-directories              '("/node_modules" "/venv" "/.venv" "/.cask")
-          treemacs-show-cursor                     nil
-          treemacs-show-hidden-files               t
-          treemacs-silent-filewatch                nil
-          treemacs-silent-refresh                  nil
-          treemacs-sorting                         'alphabetic-asc
-          treemacs-select-when-already-in-treemacs 'move-back
-          treemacs-space-between-root-nodes        t
-          treemacs-tag-follow-cleanup              t
-          treemacs-tag-follow-delay                1.5
-          treemacs-text-scale                      nil
-          treemacs-user-mode-line-format           nil
-          treemacs-user-header-line-format         nil
-          treemacs-wide-toggle-width               70
-          treemacs-width                           35
-          treemacs-width-increment                 1
-          treemacs-width-is-initially-locked       t
-          treemacs-workspace-switch-cleanup        nil)
-
-    (treemacs-follow-mode nil)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode 'always)
-    (when treemacs-python-executable
-      (treemacs-git-commit-diff-mode t))
-
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple)))
-
-    (treemacs-hide-gitignored-files-mode nil))
-  :bind
-  (:map global-map
-        ("M-0"       . #'treemacs-select-window)
-        ("C-x t 1"   . #'treemacs-delete-other-windows)
-        ("C-x t t"   . #'treemacs)
-        ("C-x t d"   . #'treemacs-select-directory)
-        ("C-x t B"   . #'treemacs-bookmark)
-        ("C-x t C-t" . #'treemacs-find-file)
-        ("C-x t M-t" . #'treemacs-find-tag)))
+  (defun $neotree--project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root.")))))
 
 (use-package nerd-icons)
-
-(use-package treemacs-nerd-icons
-  :config
-  (treemacs-load-theme "nerd-icons"))
-
-(use-package treemacs-evil
-  :after (treemacs evil))
-
-(use-package treemacs-projectile
-  :after (treemacs projectile))
-
-(use-package treemacs-icons-dired
-  :hook (dired-mode-hook . treemacs-icons-dired-enable-once))
-
-(use-package treemacs-magit
-  :after (treemacs magit))
-
-(use-package treemacs-tab-bar ;;treemacs-tab-bar if you use tab-bar-mode
-  :after (treemacs)
-  :config (treemacs-set-scope-type 'Tabs))
 
 (use-package rg
   :commands (rg)
@@ -1239,6 +1150,17 @@ no matter what."
   :config
   (flycheck-add-mode 'javascript-eslint 'web-mode)
   (flycheck-add-mode 'typescript-tslint 'web-mode)
+  (defun $flycheck--configure-python-checkers ()
+    (flycheck-add-next-checker 'lsp 'python-pylint))
+  (defun $flycheck--configure-json-checkers ()
+    (flycheck-add-next-checker 'lsp 'json-jsonlint))
+  (defun $flycheck--configure-ts-checkers ()
+    (flycheck-add-next-checker 'lsp 'javascript-eslint))
+  :hook ((python-mode-hook . $flycheck--configure-python-checkers)
+         (python-ts-mode-hook . $flycheck--configure-python-checkers)
+         (json-js-mode-hook . $flycheck--configure-json-checkers)
+         (json-ts-mode-hook . $flycheck--configure-json-checkers)
+         (typescript-ts-mode-hook . $flycheck--configure-ts-checkers))
   :init
   (global-flycheck-mode t))
 
