195cef993f4bd354898b0a0592355ef7460a746b
Scott F <sfrieds3@gmail.com>
2023-11-12 08:06:03 -0600

    hammerspoon: config updates

--- roles/dotfiles/files/hammerspoon/init.lua
+++ roles/dotfiles/files/hammerspoon/init.lua
@@ -1,1 +1,19 @@
 require("reload")
+local focus = require("focus")
+
+local browser = "firefox"
+local terminal = "alacritty"
+local editor = "Emacs"
+
+hs.hotkey.bind({ "cmd", "shift" }, "j", function()
+  focusandback(editor)
+end)
+hs.hotkey.bind({ "cmd", "shift" }, "k", function()
+  focusandback(browser)
+end)
+hs.hotkey.bind({ "cmd", "shift" }, "h", function()
+  focusandback(terminal)
+end)
+hs.hotkey.bind({ "cmd", "shift" }, "n", function()
+  focusandback("insomnia")
+end)
