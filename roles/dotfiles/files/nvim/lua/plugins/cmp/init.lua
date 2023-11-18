a7fad027daf159950ca0e8c0380b17ce577b5d08
Scott F <s@sfrieds3.net>
2023-11-17 23:25:06 -0600

    nvim: remove tags cmp sourse

--- roles/dotfiles/files/nvim/lua/plugins/cmp/init.lua
+++ roles/dotfiles/files/nvim/lua/plugins/cmp/init.lua
@@ -10,7 +10,6 @@ return {
       "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-nvim-lua",
       "hrsh7th/cmp-path",
-      "quangnguyen30192/cmp-nvim-tags",
       "saadparwaiz1/cmp_luasnip",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lsp-signature-help",
