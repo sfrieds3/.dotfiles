6913fb247e2c0d898254ac9251a0a99f8289a850
Scott F <sfrieds3@gmail.com>
2023-12-04 09:19:35 -0600

    hammerspoon: some config updates

--- roles/dotfiles/files/hammerspoon/init.lua
+++ roles/dotfiles/files/hammerspoon/init.lua
@@ -1,9 +1,18 @@
 require("reload")
 local focus = require("focus")
+local switchscreen = require("switchscreen")
 
+--
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "f3", function()
+  hs.console.alpha(0.9)
+  hs.toggleConsole()
+end)
+
+-- focus windows
 local browser = "firefox"
 local terminal = "kitty"
 local api_query = "Bruno"
+local passwords = "1Password"
 
 hs.hotkey.bind({ "cmd", "shift" }, "a", function()
   focusandback("Safari")
@@ -20,9 +29,6 @@ end)
 hs.hotkey.bind({ "cmd", "shift" }, "g", function()
   focusandback("Google Chrome")
 end)
-hs.hotkey.bind({ "cmd", "shift" }, ",", function()
-  focusandback(terminal)
-end)
 hs.hotkey.bind({ "cmd", "shift" }, "k", function()
   focusandback("PyCharm Professional Edition")
 end)
@@ -38,6 +44,39 @@ end)
 hs.hotkey.bind({ "cmd", "shift" }, "s", function()
   focusandback("Spotify")
 end)
+hs.hotkey.bind({ "cmd", "shift" }, ",", function()
+  focusandback(terminal)
+end)
 hs.hotkey.bind({ "cmd", "shift" }, ";", function()
   focusandback("Reminders")
+end)
+hs.hotkey.bind({ "cmd", "shift" }, "'", function()
+  focusandback(passwords)
+end)
+
+-- switch screens quickly
+hs.hotkey.bind({ "ctrl", "cmd" }, "]", function()
+  switchscreen.focusScreen(hs.mouse.getCurrentScreen():next())
+end)
+hs.hotkey.bind({ "ctrl", "cmd" }, "[", function()
+  switchscreen.focusScreen(hs.mouse.getCurrentScreen():previous())
+end)
+
+-- move window to screen
+local function windowToNextScreen()
+  local app = hs.window.focusedWindow()
+  app:moveToScreen(app:screen():next())
+  app:maximize()
+end
+local function windowToPreviousScreen()
+  local app = hs.window.focusedWindow()
+  app:moveToScreen(app:screen():previous())
+  app:maximize()
+end
+
+hs.hotkey.bind({ "ctrl", "cmd", "shift" }, "]", function()
+  windowToNextScreen()
+end)
+hs.hotkey.bind({ "ctrl", "cmd", "shift" }, "[", function()
+  windowToPreviousScreen()
 end)
