ff898f32dc967eb4b124fb686fe53508fd561d68
Scott F <sfrieds3@gmail.com>
2023-12-04 18:15:46 -0600

    go: config updates for golang

--- roles/dotfiles/files/nvim/after/ftplugin/go.lua
+++ roles/dotfiles/files/nvim/after/ftplugin/go.lua
@@ -1,9 +1,10 @@
 local bufnr = vim.api.nvim_get_current_buf()
 
 vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 8
+vim.bo.softtabstop = 2
+vim.bo.tabstop = 2
 
-vim.api.nvim_create_autocmd("BufWritePost", {
+vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function(args)
     require("conform").format({ formatters = { "gofmt", "goimports" }, bufnr = args.bufnr })
   end,
--- roles/dotfiles/files/nvim/lua/plugins/lsp/config.lua
+++ roles/dotfiles/files/nvim/lua/plugins/lsp/config.lua
@@ -58,9 +58,6 @@ function M.setup()
   lspconfig.gopls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    cmd = { "gopls", "serve" },
-    filetypes = { "go", "gomod" },
-    root_dir = require("lspconfig.util").root_pattern("go.work", "go.mod", ".git"),
     settings = {
       gopls = {
         analyses = {
@@ -79,31 +76,6 @@ function M.setup()
         },
       },
     },
-  })
-
-  local function org_imports(wait_ms)
-    local params = vim.lsp.util.make_range_params()
-    params.context = { only = { "source.organizeImports" } }
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
-    for _, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
-        else
-          vim.lsp.buf.execute_command(r.command)
-        end
-      end
-    end
-  end
-
-  vim.api.nvim_create_augroup("Golang", { clear = true })
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    group = "Golang",
-    pattern = "*.go",
-    callback = function()
-      vim.lsp.buf.format({ timeout_ms = 1000 })
-      org_imports(1000)
-    end,
   })
 
   local runtime_path = vim.split(package.path, ";", {})
