d22d6013117ef98fa11b9b53636f130b455d2dad
Scott F <sfrieds3@gmail.com>
2023-12-29 23:23:18 -0600

    nvim: luasnip updates

--- roles/dotfiles/files/nvim/lua/plugins/code.lua
+++ roles/dotfiles/files/nvim/lua/plugins/code.lua
@@ -4,13 +4,14 @@ return {
   {
     "L3MON4D3/LuaSnip",
     event = "InsertEnter",
+    build = "make install_jsregexp",
     dependencies = {
       { "rafamadriz/friendly-snippets" },
     },
 
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
-      require("luasnip.loaders.from_lua").lazy_load({ paths = vim.fn.stdpath("data") .. "/snippets_local" })
+      require("luasnip.loaders.from_lua").lazy_load({ paths = { vim.fn.stdpath("data") .. "/snippets_local" } })
     end,
   },
   {
