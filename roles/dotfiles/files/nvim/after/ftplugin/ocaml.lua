f1d90ca4dd7781f2caed501753a9c9764fef79df
Scott F <sfrieds3@gmail.com>
2024-01-04 22:28:05 -0600

    nvim: update ocaml ftplugin

--- roles/dotfiles/files/nvim/after/ftplugin/ocaml.lua
+++ roles/dotfiles/files/nvim/after/ftplugin/ocaml.lua
@@ -1,4 +1,14 @@
+local augroup = require("utils.utils").augroup
+
 vim.bo.shiftwidth = 2
 
 -- TODO: call module name here
-vim.bo.makeprg = "dune exec"
+vim.bo.makeprg = "dune build"
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(args)
+    require("conform").format({ formatters = { "ocamlformat" }, bufnr = args.bufnr })
+  end,
+  buffer = vim.api.nvim_get_current_buf(),
+  group = augroup("ocamlformat:" .. vim.api.nvim_get_current_buf()),
+})
