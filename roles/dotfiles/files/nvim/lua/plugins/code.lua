3ee26d0a8d2db0e24d8d72723d9043d5d25e5da0
Scott F <sfrieds3@gmail.com>
2024-01-18 17:29:56 -0600

    nvim: update config for ufo

--- roles/dotfiles/files/nvim/lua/plugins/code.lua
+++ roles/dotfiles/files/nvim/lua/plugins/code.lua
@@ -55,24 +55,54 @@ return {
 
     --stylua: ignore
     keys = {
-      { "zR", function() require("ufo").openAllFolds() end },
-      { "zM", function() require("ufo").closeAllFolds() end },
+      { "zR", function() require("ufo").openAllFolds() end, desc = "UFO Open All Folds" },
+      { "zM", function() require("ufo").closeAllFolds() end, desc = "UFO Close All Folds" },
       {
-        "K",
+        "<leader>cp",
         function()
           local winid = require("ufo").peekFoldedLinesUnderCursor()
           if not winid then
             vim.lsp.buf.hover()
           end
         end,
+        desc = "UFO Peek Folded Lines Under Cursor"
       },
     },
 
     config = function()
+      local handler = function(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, "MoreMsg" })
+        return newVirtText
+      end
+
       require("ufo").setup({
         provider_selector = function(bufnr, filetype, buftype)
           return { "treesitter", "indent" }
         end,
+        fold_virt_text_handler = handler,
       })
     end,
   },
