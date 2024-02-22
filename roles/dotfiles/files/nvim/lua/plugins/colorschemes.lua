1221daecdaa2b0340a95e40aa078c5503c9b45f9
Scott F <sfrieds3@gmail.com>
2024-02-22 09:27:08 -0600

    nvim (colorschemes): clean up some unused colorschemes

--- roles/dotfiles/files/nvim/lua/plugins/colorschemes.lua
+++ roles/dotfiles/files/nvim/lua/plugins/colorschemes.lua
@@ -4,8 +4,6 @@ return {
       enabled = false,
     },
   } },
-  { "sainnhe/edge", event = "VeryLazy" },
-  { "nyoom-engineering/oxocarbon.nvim", event = "VeryLazy" },
   { "loctvl842/monokai-pro.nvim", event = "VeryLazy" },
   {
     "projekt0n/github-nvim-theme",
@@ -91,40 +89,6 @@ return {
       --   },
       -- },
     },
-  },
-  {
-    "NTBBloodbath/doom-one.nvim",
-    event = "VeryLazy",
-
-    config = function()
-      -- Add color to cursor
-      vim.g.doom_one_cursor_coloring = false
-      -- Set :terminal colors
-      vim.g.doom_one_terminal_colors = true
-      -- Enable italic comments
-      vim.g.doom_one_italic_comments = true
-      -- Enable TS support
-      vim.g.doom_one_enable_treesitter = true
-      -- Color whole diagnostic text or only underline
-      vim.g.doom_one_diagnostics_text_color = false
-      -- Enable transparent background
-      vim.g.doom_one_transparent_background = false
-
-      -- Pumblend transparency
-      vim.g.doom_one_pumblend_enable = false
-      vim.g.doom_one_pumblend_transparency = 20
-
-      -- Plugins integration
-      vim.g.doom_one_plugin_telescope = true
-      vim.g.doom_one_plugin_neogit = true
-      vim.g.doom_one_plugin_nvim_tree = true
-      vim.g.doom_one_plugin_vim_illuminate = true
-      vim.g.doom_one_plugin_neo_tree = true
-
-      -- Pumblend transparency
-      vim.g.doom_one_pumblend_enable = true
-      vim.g.doom_one_pumblend_transparency = 20
-    end,
   },
   {
     "sainnhe/gruvbox-material",
@@ -262,10 +226,6 @@ return {
     end,
   },
   {
-    "olimorris/onedarkpro.nvim",
-    event = "VeryLazy",
-  },
-  {
     "sainnhe/sonokai",
     event = "VeryLazy",
     config = function()
@@ -289,20 +249,6 @@ return {
         variables = {},
         sidebars = "normal",
         floats = "transparent",
-      },
-    },
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    event = "VeryLazy",
-    opts = {
-      colorblind = {
-        enable = true,
-        severity = {
-          protan = 1,
-          deutan = 1,
-          tritan = 0,
-        },
       },
     },
   },
