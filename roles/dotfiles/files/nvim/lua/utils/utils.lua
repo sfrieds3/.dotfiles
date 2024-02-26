bc6b3d9016d7a78082b04d77370cfad1b8ecbcb5
Scott F <sfrieds3@gmail.com>
2024-02-26 08:30:01 -0600

    nvim (utils): update utils

--- roles/dotfiles/files/nvim/lua/utils/utils.lua
+++ roles/dotfiles/files/nvim/lua/utils/utils.lua
@@ -1,25 +1,26 @@
-local M = {}
-local vo = vim.o
+local Utils = {}
 
-function M.remove_key(tbl, key)
+function Utils.remove_key(tbl, key)
   local elem = tbl[key]
   tbl[key] = nil
   return elem
 end
 
-function M.opt(o, v, scopes)
-  scopes = scopes or { vo }
-  for _, s in ipairs(scopes) do
-    s[o] = v
-  end
-end
-
-function M.t(str)
+function Utils.t(str)
   -- Adjust boolean arguments as needed
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-function M.mapper(opts)
+function Utils.table_contains(table, key)
+  for _, value in pairs(table) do
+    if value == key then
+      return true
+    end
+  end
+  return false
+end
+
+function Utils.mapper(opts)
   -- return function with default opts to map keybindings
   -- opts default to noremap if opts=nil
   opts = opts or { noremap = true }
@@ -29,23 +30,7 @@ function M.mapper(opts)
   return map
 end
 
-function M.source_dir(dir)
-  -- inspiration: https://github.com/tjdevries/astronauta.nvim/blob/master/lua/astronauta/plugin.lua
-  local source_path = string.format("%s/**/*.lua", dir)
-  for _, mod in ipairs(vim.api.nvim_get_runtime_file(source_path, true)) do
-    local ok, msg = pcall(loadfile, mod)
-    if not ok then
-      print("Failed to load: ", mod)
-      print("\t", msg)
-    end
-  end
-end
-
-function M.isempty(s)
-  return s == nil or s == ""
-end
-
-function M.get_buf_option(opt)
+function Utils.get_buf_option(opt)
   local status_ok, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
   if not status_ok then
     return nil
@@ -54,61 +39,21 @@ function M.get_buf_option(opt)
   end
 end
 
--- from folke/LazyVim
--- returns the root directory based on:
--- * lsp workspace folders
--- * lsp root_dir
--- * root pattern of filename of the current buffer
--- * root pattern of cwd
----@return string
-function M.get_root()
-  ---@type string?
-  local path = vim.api.nvim_buf_get_name(0)
-  path = path ~= "" and vim.loop.fs_realpath(path) or nil
-  ---@type string[]
-  local roots = {}
-  if path then
-    for _, client in pairs(vim.lsp.get_clients({ bufnr = 0 })) do
-      local workspace = client.config.workspace_folders
-      local paths = workspace and vim.tbl_map(function(ws)
-        return vim.uri_to_fname(ws.uri)
-      end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
-      for _, p in ipairs(paths) do
-        local r = vim.loop.fs_realpath(p)
-        if path:find(r, 1, true) then
-          roots[#roots + 1] = r
-        end
-      end
-    end
-  end
-  table.sort(roots, function(a, b)
-    return #a > #b
-  end)
-  ---@type string?
-  local root = roots[1]
-  if not root then
-    path = path and vim.fs.dirname(path) or vim.loop.cwd()
-    ---@type string?
-    root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
-    root = root and vim.fs.dirname(root) or vim.loop.cwd()
-  end
-  ---@cast root string
-  return root
-end
-
 -- Recursively iterate through highlight until
 -- we do not return a link
 ---@param name string highlight name
-function M.recursive_get_highlight(name)
+function Utils.recursive_get_highlight(name)
   local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
   if hl["link"] then
-    M.recursive_get_highlight(hl["link"])
+    Utils.recursive_get_highlight(hl["link"])
   else
     return hl
   end
 end
 
-function M.augroup(name, check_dupe)
+_G.sfrieds3_autocmd_registry = {}
+
+function Utils.augroup(name, check_dupe)
   local group_name = "sfrieds3:" .. name
   local should_check_dupe = check_dupe or true
   if should_check_dupe then
@@ -119,7 +64,13 @@ function M.augroup(name, check_dupe)
     end
   end
 
-  return vim.api.nvim_create_augroup(group_name, { clear = true })
+  local augroup = vim.api.nvim_create_augroup(group_name, { clear = true })
+
+  if not Utils.table_contains(_G.sfrieds3_autocmd_registry, augroup) then
+    _G.sfrieds3_autocmd_registry[augroup] = true
+  end
+
+  return augroup
 end
 
-return M
+return Utils
