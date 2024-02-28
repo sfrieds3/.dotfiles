2fe4e36b64d5c69342e0dad97d94f98e6fb52abc
Scott F <sfrieds3@gmail.com>
2024-02-28 14:59:12 -0600

    nvim: add iron.nvim

--- roles/dotfiles/files/nvim/lua/plugins/code.lua
+++ roles/dotfiles/files/nvim/lua/plugins/code.lua
@@ -2,6 +2,48 @@ local is_executable = vim.fn.executable
 
 return {
   {
+    "Vigemus/iron.nvim",
+    config = function()
+      local iron = require("iron.core")
+      iron.setup({
+        config = {
+          scratch_repl = true,
+          repl_definition = {
+            sh = {
+              command = { "zsh" },
+            },
+          },
+          -- repl_open_cmd = require("iron.view").right(function()
+          --   return math.floor(vim.o.columns * 0.4)
+          -- end),
+          repl_open_cmd = "vsplit",
+        },
+        keymaps = {
+          send_motion = "\\sc",
+          visual_send = "\\sc",
+          send_file = "\\sf",
+          send_line = "\\sl",
+          send_until_cursor = "\\su",
+          send_mark = "\\sm",
+          mark_motion = "\\mc",
+          mark_visual = "\\mc",
+          remove_mark = "\\md",
+          cr = "\\s<cr>",
+          interrupt = "\\s<space>",
+          exit = "\\sq",
+          clear = "\\cl",
+        },
+        highlight = {
+          italic = true,
+        },
+        ignore_blank_lines = true,
+      })
+
+      vim.keymap.set("n", "\\i", "<Cmd>IronRepl<CR>")
+      vim.keymap.set("n", "\\I", "<Cmd>IronReplHere<CR>")
+    end,
+  },
+  {
     "L3MON4D3/LuaSnip",
     event = "InsertEnter",
     build = "make install_jsregexp",
