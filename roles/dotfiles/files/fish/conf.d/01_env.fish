55bcf9848f23b880de9f7054c81d61b887959eec
Scott F <sfrieds3@gmail.com>
2024-03-28 08:53:47 -0500

    fish: add PIP_REQUIRE_VIRTUALENV

--- roles/dotfiles/files/fish/conf.d/01_env.fish
+++ roles/dotfiles/files/fish/conf.d/01_env.fish
@@ -3,6 +3,7 @@ if status is-interactive
     set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
     set -gx PYTHON3_VENV $PYTHON3_VENV_BIN/python3
     set -gx VIRTUAL_ENV_DISABLE_PROMPT 1
+    set -gx PIP_REQUIRE_VIRTUALENV 1
 
     set -gx FZF_DEFAULT_COMMAND "fd -t f --hidden --absolute-path"
     # set -gx FZF_DEFAULT_OPTS='--color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672'
