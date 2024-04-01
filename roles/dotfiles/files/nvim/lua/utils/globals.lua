d155241019749d29241906d197ada53522b79c01
Scott F <sfrieds3@gmail.com>
2024-04-01 11:13:08 -0500

    nvim: add gobal logger

--- roles/dotfiles/files/nvim/lua/utils/globals.lua
+++ roles/dotfiles/files/nvim/lua/utils/globals.lua
@@ -3,7 +3,26 @@ P = function(v)
   return v
 end
 
-Dump = function(...)
+DUMP = function(...)
   local objects = vim.tbl_map(vim.inspect, { ... })
   print(unpack(objects))
+end
+
+---@alias Logger table<string, table>
+
+---@type Logger
+local loggers = {}
+
+--- Get a plenary logger for level
+---@param level string? log level
+---@return Logger
+LOGGER = function(level)
+  level = level or "info"
+  local _logger = loggers[level]
+  if not _logger then
+    _logger = require("plenary.log"):new({ level = level })
+    loggers[level] = _logger
+  end
+
+  return _logger
 end
