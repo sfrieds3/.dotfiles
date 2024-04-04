6a7bf02496b107ce325e4daddd3278ba5d280b91
Scott F <s@sfrieds3.net>
2024-04-03 21:05:37 -0500

    nvim (lsp): remove unnecessary client capabilities config

--- roles/dotfiles/files/nvim/lua/plugins/lsp/config.lua
+++ roles/dotfiles/files/nvim/lua/plugins/lsp/config.lua
@@ -3,27 +3,11 @@ local lspconfig = require("lspconfig")
 
 function M.setup()
   local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-  -- capabilities.textDocument.codeLens = true
-  capabilities.textDocument.completion.completionItem.snippetSupport = true
-  capabilities.textDocument.completion.completionItem.preselectSupport = true
-  capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-  capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-  capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-  capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-  capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-  capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
-    },
-  }
 
   local function on_attach(client, bufnr)
     if client.server_capabilities.documentSymbolProvider then
       require("nvim-navic").attach(client, bufnr)
     end
-
     require("config.statusline").init_lsp_progress()
   end
 
