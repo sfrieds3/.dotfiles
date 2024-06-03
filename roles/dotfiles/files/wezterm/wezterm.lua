4da8e8eaa0d760cb4e3aad93bc8ab7b36c3c509b
Scott F <sfrieds3@gmail.com>
2024-06-03 09:10:11 -0500

    font: add commit mono

--- roles/dotfiles/files/wezterm/wezterm.lua
+++ roles/dotfiles/files/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = {
   },
   color_scheme = "Gruvbox dark, pale (base16)",
 
-  font = wezterm.font("IBM Plex Mono", { weight = "Regular" }),
+  font = wezterm.font("Commit Mono", { weight = "Regular" }),
   line_height = 1.1,
   font_size = 11,
   use_cap_height_to_scale_fallback_fonts = true,
