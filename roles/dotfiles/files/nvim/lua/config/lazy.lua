2e621cd58739ac8adf3dd7ba7e50253b20d52378
Scott F <s@sfrieds3.net>
2024-06-24 21:35:48 -0400

    nvim (lazy): update config for v11.0

--- roles/dotfiles/files/nvim/lua/config/lazy.lua
+++ roles/dotfiles/files/nvim/lua/config/lazy.lua
@@ -2,19 +2,14 @@ local M = {}
 
 function M.init_lazy()
   local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "--single-branch",
-      "https://github.com/folke/lazy.nvim.git",
-      lazypath,
-    })
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   end
+  vim.opt.rtp:prepend(lazypath)
   vim.opt.runtimepath:prepend(lazypath)
   require("lazy").setup("plugins", {
-    install = { colorscheme = { "mellifluous", "habamax" } },
+    install = { colorscheme = { "kanagawa", "habamax" } },
     dev = {
       path = "~/dev/personal",
       patterns = { "sfrieds3" },
@@ -25,6 +20,19 @@ function M.init_lazy()
     },
     diff = {
       cmd = "diffview.nvim",
+    },
+    pkg = {
+      enabled = true,
+      cache = vim.fn.stdpath("state") .. "/lazy/pkg-cache.lua",
+      sources = {
+        "lazy",
+        "rockspec",
+        "packspec",
+      },
+    },
+    rocks = {
+      root = vim.fn.stdpath("data") .. "/lazy-rocks",
+      server = "https://nvim-neorocks.github.io/rocks-binaries/",
     },
   })
 end
