3be870e9870adb9ac0d6e477f69353b6ba62a6d3
Scott F <sfrieds3@gmail.com>
2024-07-03 11:01:36 -0500

    wezterm: update config

--- roles/dotfiles/files/wezterm/wezterm.lua
+++ roles/dotfiles/files/wezterm/wezterm.lua
@@ -1,46 +1,50 @@
 local wezterm = require("wezterm")
 require("utils.workspace")
 
-local config = {
-  default_prog = {
-    "/opt/homebrew/bin/fish",
-    "--login",
-  },
-  -- color_scheme = "Gruvbox dark, pale (base16)",
-  color_scheme = "kanagawabones",
+local config = {}
 
-  font = wezterm.font("Berkeley Mono", { weight = "Regular" }),
-  line_height = 1.1,
-  font_size = 11,
-  use_cap_height_to_scale_fallback_fonts = true,
-  freetype_load_flags = "NO_HINTING",
-  freetype_load_target = "Normal",
-  front_end = "OpenGL",
-
-  automatically_reload_config = true,
-  disable_default_key_bindings = true,
-  native_macos_fullscreen_mode = true,
-  use_fancy_tab_bar = false,
-  show_new_tab_button_in_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = false,
-  status_update_interval = 1000,
-  tab_max_width = 60,
-  tab_bar_at_bottom = true,
-  scrollback_lines = 1000000,
-  audible_bell = "Disabled",
-  window_decorations = "RESIZE",
-  window_close_confirmation = "AlwaysPrompt",
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
-
-  -- mux config
-  -- unix_domains = { { name = "unix" } },
-  -- default_gui_startup_args = { "connect", "unix" },
+config.default_prog = {
+  "/opt/homebrew/bin/fish",
+  "--login",
 }
+-- color_scheme = "Gruvbox dark, pale (base16)",
+config.color_scheme = "kanagawabones"
+
+config.font = wezterm.font("Berkeley Mono", { weight = "Regular" })
+config.line_height = 1.1
+config.font_size = 11
+config.use_cap_height_to_scale_fallback_fonts = true
+config.freetype_load_flags = "NO_HINTING"
+config.freetype_load_target = "Normal"
+config.front_end = "OpenGL"
+
+config.automatically_reload_config = true
+config.disable_default_key_bindings = true
+config.native_macos_fullscreen_mode = true
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.status_update_interval = 1000
+config.tab_max_width = 60
+config.tab_bar_at_bottom = true
+config.scrollback_lines = 1000000
+config.audible_bell = "Disabled"
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "AlwaysPrompt"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.quick_select_patterns = {
+  "([[:ascii:]].*-)[[:ascii:]].*[[:space:]]", -- kubernetes pod names
+}
+
+-- mux config
+-- unix_domains = { { name = "unix" } },
+-- default_gui_startup_args = { "connect", "unix" },
 
 wezterm.on("window-config-reloaded", function(window, pane)
   window:toast_notification("wezterm", "configuration reloaded!", nil, 4000)
