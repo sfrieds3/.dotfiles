2ca6ff4be3fea24f93a8cf95700e25234f8481b4
Scott F <sfrieds3@gmail.com>
2024-07-08 10:57:55 -0500

    nvim (json): add commentstring

--- roles/dotfiles/files/nvim/after/ftplugin/json.lua
+++ roles/dotfiles/files/nvim/after/ftplugin/json.lua
@@ -3,6 +3,7 @@ local bufnr = vim.api.nvim_get_current_buf()
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 vim.bo.formatprg = "jq"
+vim.bo.commentstring = "// %s"
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   callback = function(args)
