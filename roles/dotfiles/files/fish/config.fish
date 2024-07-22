e9080281cc103c23e52cf64c0d77fe170834afc4
Scott F <sfrieds3@gmail.com>
2024-07-22 17:42:55 -0500

    fish: opam configuration

--- roles/dotfiles/files/fish/config.fish
+++ roles/dotfiles/files/fish/config.fish
@@ -57,3 +57,12 @@ if status is-interactive
 
     fish_set_virtual_env
 end
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/Users/scott/.opam/opam-init/init.fish' && source '/Users/scott/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
