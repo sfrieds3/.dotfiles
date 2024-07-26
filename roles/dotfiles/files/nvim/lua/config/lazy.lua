6611fa7e613391ffd424a418cd5a5fc4dda5f858
Scott F <sfrieds3@gmail.com>
2024-07-26 07:55:17 -0500

    nvim (lazy): update list of preferred colorschemes

--- roles/dotfiles/files/nvim/lua/config/lazy.lua
+++ roles/dotfiles/files/nvim/lua/config/lazy.lua
@@ -9,7 +9,7 @@ function M.init_lazy()
   vim.opt.rtp:prepend(lazypath)
   vim.opt.runtimepath:prepend(lazypath)
   require("lazy").setup("plugins", {
-    install = { colorscheme = { "catppuccin", "tokyonight", "kanagawa", "habamax" } },
+    install = { colorscheme = { "kanagawa", "catppuccin", "habamax" } },
     dev = {
       path = "~/code/personal",
       patterns = { "sfrieds3" },
