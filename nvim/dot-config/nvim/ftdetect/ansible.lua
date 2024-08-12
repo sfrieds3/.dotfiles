64af9a7120014a669dbcd402fbe4ee6af7c3565f
Scott F <sfrieds3@gmail.com>
2024-08-12 10:00:15 -0500

    Add ansible ftdetect

---
+++ nvim/dot-config/nvim/ftdetect/ansible.lua
@@ -0,0 +1,32 @@
+if vim.filetype then
+  vim.filetype.add({
+    pattern = {
+      [".*/host_vars/.*%.ya?ml"] = "yaml.ansible",
+      [".*/group_vars/.*%.ya?ml"] = "yaml.ansible",
+      [".*/group_vars/.*/.*%.ya?ml"] = "yaml.ansible",
+      [".*/playbooks/.*%.ya?ml"] = "yaml.ansible",
+      [".*/roles/.*/tasks/.*%.ya?ml"] = "yaml.ansible",
+      [".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
+    },
+  })
+else
+  vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = {
+      "*/host_vars/*.yml",
+      "*/host_vars/*.yaml",
+      "*/group_vars/*.yml",
+      "*/group_vars/*.yaml",
+      "*/group_vars/*/*.yml",
+      "*/group_vars/*/*.yaml",
+      "*/playbooks/*.yml",
+      "*/playbooks/*.yaml",
+      "*/roles/*/tasks/*.yml",
+      "*/roles/*/tasks/*.yaml",
+      "*/roles/*/handlers/*.yml",
+      "*/roles/*/handlers/*.yaml",
+    },
+    callback = function()
+      vim.bo.filetype = "yaml.ansible"
+    end,
+  })
+end
