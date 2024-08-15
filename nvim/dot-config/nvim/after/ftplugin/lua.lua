b7ca576c38d45af10a7993d9d17c2df85571f721
Scott F <sfrieds3@gmail.com>
2024-08-14 22:04:59 -0500

    nvim (lua): remove r formatoption

--- nvim/dot-config/nvim/after/ftplugin/lua.lua
+++ nvim/dot-config/nvim/after/ftplugin/lua.lua
@@ -4,6 +4,7 @@ local bufnr = vim.api.nvim_get_current_buf()
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 vim.bo.expandtab = true
+vim.opt_local.formatoptions:remove("r")
 
 if is_executable("stylua") then
   vim.api.nvim_create_autocmd("BufWritePre", {
