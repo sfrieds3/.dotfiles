75cc40c0f493bbe7766503d8e55475b7427937f3
Scott F <sfrieds3@gmail.com>
2024-08-17 18:07:46 -0500

    nvim: personal plugins into one file

--- nvim/dot-config/nvim/lua/plugins/sfrieds3.lua
+++ nvim/dot-config/nvim/lua/plugins/sfrieds3.lua
@@ -3,4 +3,12 @@ return {
     "sfrieds3/easynote.nvim",
     opts = {},
   },
+  {
+    "sfrieds3/pynvenv.nvim",
+    ft = "python",
+    cmd = { "PynvnevWorkonVenv", "PynvenvActivateVenv", "PynvenvActivateVenvAlias", "PynvenvActivateProjectVenv" },
+    opts = {
+      workon_home = "$HOME/.venv",
+    },
+  },
 }
