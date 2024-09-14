f57539fc1f9f223758ea69052ad9dd62329650a5
Scott F <s@sfrieds3.net>
2024-09-14 15:23:55 -0500

    nvim (test): do not open neotest output on run

--- nvim/dot-config/nvim/lua/plugins/test/init.lua
+++ nvim/dot-config/nvim/lua/plugins/test/init.lua
@@ -41,7 +41,7 @@ return {
         },
       },
       status = { virtual_text = true },
-      output = { enabled = true, open_on_run = true },
+      output = { enabled = true, open_on_run = false },
       quickfix = {
         open = function()
           require("trouble").open({ mode = "quickfix", focus = false })
