31b7742eff441d77a4da3d099981f9a8a2e45100
Scott F <s@sfrieds3.net>
2024-10-03 22:22:41 -0500

    nvim (python): more work on pytest fixtures plugin

--- nvim/dot-config/nvim/lua/sfrieds3/pytest_fixtures.lua
+++ nvim/dot-config/nvim/lua/sfrieds3/pytest_fixtures.lua
@@ -26,11 +26,9 @@ local file = "~/code/prefect/setup.py"
 local project_root = get_project_root(file)
 local project_hash = vim.fn.sha256(project_root)
 local full_path = string.format("%s/%s.json", data_path, project_hash)
-print("data path" .. full_path)
 
 local function store_fixtures(fixtures)
   Path:new(full_path):write(vim.json.encode(fixtures), "w")
-  print(string.format("Fixtures for %s stored in %s", get_project_root(file), full_path))
 end
 
 local function get_fixtures(file_path)
@@ -39,6 +37,55 @@ local function get_fixtures(file_path)
 
   local fixtures = vim.json.decode(raw_fixtures)
   return fixtures
+end
+
+local ts_query_text = [[
+(function_definition
+  name: (identifier) @function)
+  ]]
+
+local function ts_query(bufnr, current_pos)
+  local lang = "python"
+  local parser = vim.treesitter.get_parser(bufnr, lang)
+  assert(parser ~= nil, "parser should not be nil")
+  local tree = parser:parse()
+  assert(tree ~= nil, "tree should not be nil")
+  local root = tree[1]:root()
+  local query = vim.treesitter.query.parse("python", ts_query_text)
+
+  local current_row, _ = unpack(current_pos)
+
+  local offset = 0
+  local max_offset = 10
+  local nodes = {}
+  while true do
+    local start_row = math.max(current_row - offset, 0)
+    local end_row = (current_row + offset)
+
+    for id, node in query:iter_captures(root, 0, start_row, end_row) do
+      local name = query.captures[id]
+      local type = node:type()
+      table.insert(nodes, { node = node, name = name, type = type })
+    end
+
+    offset = offset + 1
+    if #nodes > 0 or offset > max_offset then
+      break
+    end
+  end
+
+  local funcs = {}
+  for _, node in ipairs(nodes) do
+    table.insert(funcs, vim.treesitter.get_node_text(node.node, bufnr))
+  end
+
+  return funcs
+end
+
+local function goto_fixture(bufnr, cursor_pos)
+  print(string.format("Get fixture for bufnr: %s cursor pos: %s", bufnr, cursor_pos))
+  local funcs = ts_query(bufnr, cursor_pos)
+  P(funcs)
 end
 
 -- Modify process_pytest_output to take input as a table
@@ -90,18 +137,31 @@ local function has_pytest()
   return vim.fn.executable("pytest") == 1
 end
 
-local function maybe_refresh_pytest_fixture_cache()
-  if get_project_root(file) and is_python(file) and has_pytest() then
+local function maybe_refresh_pytest_fixture_cache(buf_file)
+  if get_project_root(file) and is_python(buf_file) and has_pytest() then
     refresh_pytest_fixture_cache()
   end
 end
 
-local function test()
-  refresh_pytest_fixture_cache()
-end
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  group = vim.api.nvim_create_augroup("pytest_fixtures:user-commands", { clear = true }),
+  callback = function()
+    vim.api.nvim_create_user_command("PytestFixturesRefresh", function()
+      print("refreshing for " .. vim.fn.expand("%:p"))
+      maybe_refresh_pytest_fixture_cache(vim.fn.expand("%"))
+    end, {})
+  end,
+})
 
--- test()
---
--- P(get_fixtures(full_path))
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "BufWritePost" }, {
+  group = vim.api.nvim_create_augroup("pytest_fixtures:refresh", { clear = true }),
+  pattern = { "*.py", "*.pyi" },
+  callback = function(ev)
+    maybe_refresh_pytest_fixture_cache(ev.file)
+  end,
+})
 
-vim.api.nvim_create_user_command("PytestFixturesRefresh", maybe_refresh_pytest_fixture_cache, {})
+vim.keymap.set("n", "<localleader>]", function()
+  goto_fixture(vim.api.nvim_get_current_buf(), vim.api.nvim_win_get_cursor(0))
+end, { desc = "PytestFixtures Go To Fixture" })
