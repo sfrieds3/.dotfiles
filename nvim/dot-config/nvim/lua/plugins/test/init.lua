81b7786a68988dba6818638f4273e7cfe212312e
Scott F <sfrieds3@gmail.com>
2024-10-25 10:44:26 -0500

    nvim (test): fix python test args

--- nvim/dot-config/nvim/lua/plugins/test/init.lua
+++ nvim/dot-config/nvim/lua/plugins/test/init.lua
@@ -35,7 +35,7 @@ return {
     opts = {
       adapters = {
         ["neotest-python"] = {
-          dap = { justMyCode = false },
+          args = { "-vv", "-s", "--color=no" },
         },
         ["neotest-go"] = {},
         ["rustaceanvim.neotest"] = {},
@@ -102,8 +102,11 @@ return {
               local meta = getmetatable(adapter)
               if adapter.setup then
                 adapter.setup(config)
+              elseif adapter.adapter then
+                adapter.adapter(config)
+                adapter = adapter.adapter
               elseif meta and meta.__call then
-                adapter(config)
+                adapter = adapter(config)
               else
                 error("Adapter " .. name .. " does not support setup")
               end
