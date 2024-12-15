b5cdac82e530abaef70cf01b08db46c589551c55
Scott F <s@sfrieds3.net>
2024-12-14 18:08:44 -0600

    chore: cleaning up emacs stuff

--- emacs/dot-config/emacs/themes/color-theme-sanityinc-tomorrow/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-EMACS="${EMACS:=emacs}"
-
-"$EMACS" -Q -batch \
-         --eval "(progn (setq byte-compile-error-on-warn t) (push default-directory load-path))" \
-         -f batch-byte-compile \
-         ./*.el
