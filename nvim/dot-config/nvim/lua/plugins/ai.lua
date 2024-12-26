95ed0d9ed860ebdd0c586699746d2fa593c0ec8d
Scott F <sfrieds3@gmail.com>
2024-12-26 07:59:24 -0600

    nvim: disable sg.nvim

--- nvim/dot-config/nvim/lua/plugins/ai.lua
+++ nvim/dot-config/nvim/lua/plugins/ai.lua
@@ -2,17 +2,16 @@ return {
   {
     "sourcegraph/sg.nvim",
     event = "InsertEnter",
+    enabled = false,
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
   },
   {
-    {
-      "Exafunction/codeium.nvim",
-      enabled = false,
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-      },
-      opts = {},
+    "Exafunction/codeium.nvim",
+    enabled = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
+    opts = {},
   },
 }
