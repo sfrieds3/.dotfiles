e9cb9eb6a91506f5fb05abbe035c88de4d51985c
Scott F <sfrieds3@gmail.com>
2025-01-08 11:06:26 -0600

    nvim (git): update grepprg

--- nvim/dot-config/nvim/after/plugin/grep.lua
+++ nvim/dot-config/nvim/after/plugin/grep.lua
@@ -1,9 +1,9 @@
+vim.o.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+
 if vim.fn.executable("rg") then
-  vim.o.grepprg = "rg --no-heading --smart-case --vimgrep $*"
-  vim.o.grepformat = "%f:%l:%c:%m,%f:%l:%m"
+  vim.o.grepprg = "rg --no-heading --smart-case $*"
 elseif vim.fn.executable("ag") == 1 then
-  vim.o.grepprg = "ag --vimgrep $*"
-  vim.o.grepformat = "%f:%l:%c:%m"
+  vim.o.grepprg = "ag $*"
 else
   vim.opt.grepprg = "git grep -in $*"
 end
