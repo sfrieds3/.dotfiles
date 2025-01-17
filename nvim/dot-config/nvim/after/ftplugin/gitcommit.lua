fd7ee44695e736cd1854b59d6315c68c08cd167e
Scott F <sfrieds3@gmail.com>
2025-01-17 11:52:05 -0600

    nvim (ft): add `-` to iskeyword for gitcommit ft

--- nvim/dot-config/nvim/after/ftplugin/gitcommit.lua
+++ nvim/dot-config/nvim/after/ftplugin/gitcommit.lua
@@ -1,3 +1,5 @@
+vim.opt_local.iskeyword:append({ "-" })
+
 vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "git" then
