7440c7685c97064662fe921978060375d0e50dc8
Scott F <s@sfrieds3.net>
2025-01-21 08:22:54 -0600

    meta: remove nvim mason, update playbooks to install lsp/formatters/linters

--- nvim/dot-config/nvim/lua/plugins/lsp/init.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp/init.lua
@@ -4,21 +4,9 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     init = function()
       local ensure_installed = { "jsonls", "lua_ls", "pyright", "ts_ls" }
-      require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
       require("plugins.lsp.config").setup()
     end,
     dependencies = {
-      {
-        "williamboman/mason.nvim",
-        cmd = { "Mason" },
-        opts = {
-          providers = { "mason.providers.client", "mason.providers.registry-api" },
-          PATH = "append",
-        },
-        dependencies = {
-          "williamboman/mason-lspconfig.nvim",
-        },
-      },
       "b0o/SchemaStore.nvim",
       {
         "linrongbin16/lsp-progress.nvim",
