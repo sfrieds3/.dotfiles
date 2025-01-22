a697604916f3259cd0fe2b3b2b148b2cd43b0370
Scott F <sfrieds3@gmail.com>
2025-01-22 08:15:42 -0600

    nvim (mini): update mini config

--- nvim/dot-config/nvim/lua/plugins/mini.lua
+++ nvim/dot-config/nvim/lua/plugins/mini.lua
@@ -1,46 +1,36 @@
 return {
-  {
-    "echasnovski/mini.ai",
+  "echasnovski/mini.nvim",
+  version = false,
+  -- keys = {
+  --   "<localleader>sa",
+  --   "<localleader>sd",
+  --   "<localleader>sr",
+  --   "<localleader>sf",
+  --   "<localleader>sF",
+  --   "<localleader>sh",
+  --   "<localleader>sn",
+  -- },
+  config = function()
     -- keys = {
     --   { "a", mode = { "x", "o" } },
     --   { "i", mode = { "x", "o" } },
     -- },
-    version = false,
-    event = "VeryLazy",
-    opts = function()
-      local ai = require("mini.ai")
-      return {
-        n_lines = 500,
-        custom_textobjects = {
-          o = ai.gen_spec.treesitter({
-            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
-            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
-          }, {}),
-          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-          t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
-        },
-      }
-    end,
-  },
-  {
-    "echasnovski/mini.align",
-    version = false,
-    event = "VeryLazy",
-  },
-  {
-    "echasnovski/mini.surround",
-    version = false,
-    keys = {
-      "<localleader>sa",
-      "<localleader>sd",
-      "<localleader>sr",
-      "<localleader>sf",
-      "<localleader>sF",
-      "<localleader>sh",
-      "<localleader>sn",
-    },
-    opts = {
+    local ai = require("mini.ai")
+    ai.setup({
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({
+          a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+          i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }, {}),
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" },
+      },
+    })
+
+    require("mini.align").setup()
+    require("mini.surround").setup({
       mappings = {
         add = "<localleader>sa", -- Add surrounding in Normal and Visual modes
         delete = "<localleader>sd", -- Delete surrounding
@@ -53,15 +43,10 @@ return {
         suffix_last = "l", -- Suffix to search with "prev" method
         suffix_next = "n", -- Suffix to search with "next" method
       },
-    },
-  },
-  {
-    "echasnovski/mini.icons",
-    opts = function()
-      local mi = require("mini.icons")
-      mi.setup()
-      mi.mock_nvim_web_devicons()
-      return {}
-    end,
-  },
+    })
+
+    local mi = require("mini.icons")
+    mi.setup()
+    mi.mock_nvim_web_devicons()
+  end,
 }
