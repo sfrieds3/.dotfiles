1636d03b3497fccd11eefb2251e4f0c1fe5d7963
Scott F <sfrieds3@gmail.com>
2025-01-24 17:12:21 -0600

    nvim: config updates

--- nvim/dot-config/nvim/lua/plugins/editor.lua
+++ nvim/dot-config/nvim/lua/plugins/editor.lua
@@ -48,7 +48,6 @@ return {
   },
   {
     "stevearc/oil.nvim",
-    cmd = "Oil",
     event = "VeryLazy",
     opts = {
       default_file_explorer = true,
--- nvim/dot-config/nvim/lua/plugins/init.lua
+++ nvim/dot-config/nvim/lua/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
-  { "nvim-lua/plenary.nvim", lazy = true },
   {
     "folke/lazydev.nvim",
     ft = "lua",
