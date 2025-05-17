c28437b41fbdb2a0c86bd50822420bc7c809413a
Scott F <s@sfrieds3.net>
2025-05-17 13:09:54 -0400

    feat (nvim): re-add rust-analyzer config

--- nvim/dot-config/nvim/lua/plugins/lsp/config.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp/config.lua
@@ -93,6 +93,33 @@ function M.setup()
       },
     },
 
+    rust_analyzer = {
+      on_attach = on_attach,
+      capabilities = capabilities,
+      settings = {
+        ["rust-analyzer"] = {
+          cargo = {
+            allFeatures = true,
+            loadOutDirsFromCheck = true,
+            runBuildScripts = true,
+          },
+          checkOnSave = {
+            allFeatures = true,
+            command = "clippy",
+            extraArgs = { "--no-deps" },
+          },
+          procMacro = {
+            enable = true,
+            ignored = {
+              ["async-trait"] = { "async_trait" },
+              ["napi-derive"] = { "napi" },
+              ["async-recursion"] = { "async_recursion" },
+            },
+          },
+        },
+      },
+    },
+
     clangd = {
       init_options = { clangdFileStatus = true },
     },
