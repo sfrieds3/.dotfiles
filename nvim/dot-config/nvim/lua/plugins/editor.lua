be7d824b60c91cc43ecb2f407cb364f001a41791
Scott F <s@sfrieds3.net>
2025-08-03 09:25:56 -0500

    feat(nvim): add fff.nvim

--- nvim/dot-config/nvim/lua/plugins/editor.lua
+++ nvim/dot-config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,23 @@
 return {
   {
+    "dmtrKovalenko/fff.nvim",
+    build = "cargo build --release",
+    -- or if you are using nixos
+    -- build = "nix run .#release",
+    opts = {
+      -- pass here all the options
+    },
+    keys = {
+      {
+        "<leader>ff",
+        function()
+          require("fff").find_files()
+        end,
+        desc = "Toggle FFF",
+      },
+    },
+  },
+  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
