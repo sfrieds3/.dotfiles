1d768824c9de58bccd2c27019cd79951637d7d39
Scott F <s@sfrieds3.net>
2025-09-26 15:35:59 -0500

    feat(nvim): update dropbar config

--- nvim/dot-config/nvim/lua/plugins/editor.lua
+++ nvim/dot-config/nvim/lua/plugins/editor.lua
@@ -217,6 +217,37 @@ return {
       },
     },
     opts = {
+      bar = {
+        enable = function(buf, win, _)
+          buf = vim._resolve_bufnr(buf)
+          if not vim.api.nvim_buf_is_valid(buf) or not vim.api.nvim_win_is_valid(win) then
+            return false
+          end
+
+          if
+            not vim.api.nvim_buf_is_valid(buf)
+            or not vim.api.nvim_win_is_valid(win)
+            or vim.fn.win_gettype(win) ~= ""
+            or vim.wo[win].winbar ~= ""
+            or vim.bo[buf].ft == "help"
+          then
+            return false
+          end
+
+          local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+          if stat and stat.size > 1024 * 1024 then
+            return false
+          end
+
+          return vim.bo[buf].bt == "terminal"
+            or vim.bo[buf].ft == "markdown"
+            or pcall(vim.treesitter.get_parser, buf)
+            or not vim.tbl_isempty(vim.lsp.get_clients({
+              bufnr = buf,
+              method = vim.lsp.protocol.Methods.textDocument_documentSymbol,
+            }))
+        end,
+      },
       sources = {
         path = {
           modified = function(sym)
@@ -225,6 +256,21 @@ return {
               name_hl = "StatuslineDiagnosticSignError",
               icon_hl = "StatuslineDiagnosticSignError",
             })
+          end,
+
+          relative_to = function(buf, win)
+            -- Show full path in oil or fugitive buffers
+            local bufname = vim.api.nvim_buf_get_name(buf)
+            if vim.startswith(bufname, "oil://") or vim.startswith(bufname, "fugitive://") then
+              local root = bufname:gsub("^%S+://", "", 1)
+              while root and root ~= vim.fs.dirname(root) do
+                root = vim.fs.dirname(root)
+              end
+              return root
+            end
+
+            local ok, cwd = pcall(vim.fn.getcwd, win)
+            return ok and cwd or vim.fn.getcwd()
           end,
         },
       },
