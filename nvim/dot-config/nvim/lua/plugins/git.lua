a10b9cfe007afec611e183d35e0f00317fb0b87a
Scott F <s@sfrieds3.net>
2026-01-15 16:10:53 -0600

    feat(nvim): update diffview config

--- nvim/dot-config/nvim/lua/plugins/git.lua
+++ nvim/dot-config/nvim/lua/plugins/git.lua
@@ -108,6 +108,22 @@ return {
           ["q"] = "<cmd>DiffviewClose<cr>",
         },
       },
+      hooks = {
+        diff_buf_win_enter = function(bufnr, winid, ctx)
+          if ctx.layout_name:match("^diff2") then
+            if ctx.symbol == "a" then
+              vim.opt_local.winhl = table.concat({
+                "DiffAdd:DiffviewDiffAddAsDelete",
+                "DiffDelete:DiffviewDiffDelete",
+              }, ",")
+            elseif ctx.symbol == "b" then
+              vim.opt_local.winhl = table.concat({
+                "DiffDelete:DiffviewDiffDelete",
+              }, ",")
+            end
+          end
+        end,
+      },
     },
 
     keys = {
